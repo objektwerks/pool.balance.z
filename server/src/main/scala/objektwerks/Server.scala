469c98c5fa9c75c171454a4ba3ec9a5b21645b66
objektwerks <objektwerks@runbox.com>
2022-11-28 18:41:43 -0500

    refactored server

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -11,9 +11,6 @@ import zio.json.{DecoderOps, EncoderOps}
 import zio.logging.{LogFormat, file}
 
 object Server extends ZIOAppDefault:
-  val port = 7272
-  val config = ServerConfig.default.port(port)
-
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> file(Path.of("./target/server.log"))
 
@@ -21,12 +18,14 @@ object Server extends ZIOAppDefault:
     case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
   }
 
-  val app: ZIO[HttpServer, Nothing, Nothing] =
-    for
-      _      <- ZIO.log(s"HttpServer running at http://localhost:$port")
-      server <- HttpServer.serve(router)
-    yield server
+  val app: ZIO[HttpServer, Nothing, Nothing] = HttpServer.serve(router)
 
   override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
-    app
-      .provide(ServerConfig.live(config), HttpServer.live)
+    for
+      args <- getArgs
+      port = args.headOption.getOrElse("7272").toInt
+      _    <- ZIO.log(s"HttpServer running at http://localhost:$port")
+    yield
+      val config = ServerConfig.default.port(port)
+      app
+        .provide(ServerConfig.live(config), HttpServer.live)
