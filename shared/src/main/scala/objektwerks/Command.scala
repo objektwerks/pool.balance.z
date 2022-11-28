a7b930a9722daedec987b902ef39983734541d4a
objektwerks <objektwerks@runbox.com>
2022-11-28 08:06:33 -0500

    edited command

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -1,3 +1,7 @@
 package objektwerks
 
 sealed trait Command
+
+final case class ListPools() extends Command
+final case class AddPool(pool: Pool) extends Command
+final case class UpdatePool(pool: Pool) extends Command
