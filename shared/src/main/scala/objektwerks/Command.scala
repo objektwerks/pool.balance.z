5dafb847359fe9a721f0b76e7f5a94694337f95d
objektwerks <objektwerks@runbox.com>
2022-11-28 17:48:50 -0500

    refactored command

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -3,17 +3,13 @@ package objektwerks
 sealed trait Command
 
 final case class ListPools() extends Command
-final case class AddPool(pool: Pool) extends Command
-final case class UpdatePool(pool: Pool) extends Command
+final case class SavePool(pool: Pool, isNew: Boolean) extends Command
 
 final case class ListCleanings() extends Command
-final case class AddCleaning(cleaning: Cleaning) extends Command
-final case class UpdateCleaning(cleaning: Cleaning) extends Command
+final case class SaveCleaning(cleaning: Cleaning, isNew: Boolean) extends Command
 
 final case class ListMeasurements() extends Command
-final case class AddMeasurement(measurement: Measurement) extends Command
-final case class UpdateMeasurement(measurement: Measurement) extends Command
+final case class SaveMeasurement(measurement: Measurement, isNew: Boolean) extends Command
 
 final case class ListChemicals() extends Command
-final case class AddChemical(chemical: Chemical) extends Command
-final case class UpdateChemical(chemical: Chemical) extends Command
+final case class SaveChemical(chemical: Chemical, isNew: Boolean) extends Command
