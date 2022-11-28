6202e6ff0ae76da29ba3ef516706b0c40b6d6170
objektwerks <objektwerks@runbox.com>
2022-11-28 08:00:38 -0500

    built basic http servrer

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -1,3 +1,28 @@
 package objektwerks
 
-object Server
+import java.nio.file.Path
+import java.time.Instant
+
+import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.http.{!!, /, ->, Http, Request, Response, ServerConfig}
+import zio.http.{Server => HttpServer}
+import zio.http.model.Method
+import zio.json.{DecoderOps, EncoderOps}
+import zio.logging.{LogFormat, file}
+
+object Server extends ZIOAppDefault:
+  val port = 7272
+  val config = ServerConfig.default.port(port)
+
+  override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
+    Runtime.removeDefaultLoggers >>> file(Path.of("./target/server.log"))
+
+  def router: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
+    case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
+  }
+
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
+    for
+      _      <- ZIO.log(s"HttpServer running at http://localhost:$port")
+      server <- HttpServer.serve(router).provide(ServerConfig.live(config), HttpServer.live)
+    yield server
