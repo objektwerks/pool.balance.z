a252cf006933573050f67cfeacf3b3f4d8a1416d
objektwerks <objektwerks@runbox.com>
2022-11-28 06:57:21 -0500

    added frame

--- client/src/main/scala/objektwerks/Client.scala
+++ client/src/main/scala/objektwerks/Client.scala
@@ -2,15 +2,12 @@ package objektwerks
 
 import java.awt.EventQueue
 
-import javax.swing.JFrame
 import javax.swing.UIManager._
 
 object Client:
   def main(args: Array[String]): Unit =
-    EventQueue.invokeLater( new Runnable() {
-      override def run(): Unit = {
-        setLookAndFeel(getCrossPlatformLookAndFeelClassName)
-        val frame = new JFrame()
-        frame.setVisible(true)
-      }
+    EventQueue.invokeLater( () => {
+      setLookAndFeel(getCrossPlatformLookAndFeelClassName)
+      val frame = new Frame()
+      frame.setVisible(true)
     })
---
+++ client/src/main/scala/objektwerks/Frame.scala
@@ -0,0 +1,9 @@
+package objektwerks
+
+import javax.swing.{JFrame, WindowConstants}
+
+class Frame extends JFrame:
+  setTitle("Pool Balance")
+  setSize(600, 600)
+  setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  setLocationRelativeTo(null)
