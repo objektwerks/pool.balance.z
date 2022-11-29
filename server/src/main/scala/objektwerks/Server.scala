2d6cba67671e70b1ecf746535da9c77ab2b17312
objektwerks <objektwerks@runbox.com>
2022-11-29 05:37:06 -0500

    added command/event pattern match to router

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -16,6 +16,19 @@ object Server extends ZIOAppDefault:
 
   val router: Http[Any, Throwable, Request, Response] = Http.collectZIO[Request] {
     case Method.GET -> !! / "now" => ZIO.succeed( Response.text(Instant.now.toString()) )
+    case request @ Method.POST -> !! / "command" => request.body.asString.map { json =>
+      json.fromJson[Command] match
+        case Right(command) => command match // TODO!
+          case ListPools()                  => Response.json( PoolsListed(Nil).toJson )
+          case SavePool(pool)               => Response.json( PoolSaved(pool).toJson )
+          case ListCleanings()              => Response.json( CleaningsListed(Nil).toJson )
+          case SaveCleaning(cleaning)       => Response.json( CleaningSaved(cleaning).toJson )
+          case ListMeasurements()           => Response.json( MeasurementsListed(Nil).toJson )
+          case SaveMeasurement(measurement) => Response.json( MeasurementSaved(measurement).toJson )
+          case ListChemicals()              => Response.json( ChemicalsListed(Nil).toJson )
+          case SaveChemical(chemical)       => Response.json( ChemicalSaved(chemical).toJson )
+        case Left(error) => Response.json( Fault( error ).toJson )
+    }
   }
 
   override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
