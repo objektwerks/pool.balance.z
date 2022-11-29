d1c338f6446cf4c49d20b7e0b40b056846c3c97b
objektwerks <objektwerks@runbox.com>
2022-11-29 05:42:25 -0500

    refactored event

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -2,15 +2,14 @@ package objektwerks
 
 sealed trait Event
 
-object Event:
-  final case class PoolsListed(pools: List[Pool]) extends Event
-  final case class PoolSaved(pool: Pool) extends Event
+final case class PoolsListed(pools: List[Pool]) extends Event
+final case class PoolSaved(pool: Pool) extends Event
 
-  final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
-  final case class CleaningSaved(cleaning: Cleaning) extends Event
+final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
+final case class CleaningSaved(cleaning: Cleaning) extends Event
 
-  final case class MeasurementsListed(measurements: List[Measurement]) extends Event
-  final case class MeasurementSaved(measurement: Measurement) extends Event
+final case class MeasurementsListed(measurements: List[Measurement]) extends Event
+final case class MeasurementSaved(measurement: Measurement) extends Event
 
-  final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
-  final case class ChemicalSaved(chemical: Chemical) extends Event
+final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
+final case class ChemicalSaved(chemical: Chemical) extends Event
