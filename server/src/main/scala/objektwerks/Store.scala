b0b518aea246c1f766ed497f5b15114f1b10ce62
objektwerks <objektwerks@runbox.com>
2022-11-29 08:22:37 -0500

    refactored store

--- server/src/main/scala/objektwerks/Store.scala
+++ server/src/main/scala/objektwerks/Store.scala
@@ -14,21 +14,21 @@ import zio.{ZIO, ZLayer}
 case class Store(quill: Quill.Postgres[SnakeCase]):
   import quill.*
 
-  inline def addPool(pool: Pool) =
+  inline def addPool(pool: Pool): ZIO[Any, SQLException, Long] =
     run( 
       query[Pool]
         .insertValue( lift(pool) )
         .returningGenerated(_.id)
     )
 
-  inline def updatePool(pool: Pool) =
+  inline def updatePool(pool: Pool): ZIO[Any, SQLException, Long] =
     run(
       query[Pool]
         .filter(_.id == lift(pool.id))
         .updateValue( lift(pool) )
     )
 
-  inline def listPools = run( query[Pool] )
+  inline def listPools: ZIO[Any, SQLException, List[Pool]] = run( query[Pool] )
 
 object Store:
   val layer: ZLayer[Postgres[SnakeCase], Nothing, Store] = ZLayer.fromFunction(apply(_))
