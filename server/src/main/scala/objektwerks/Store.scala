fc4ef7228d26b966a9a060eaeb3ede7e89e7b2cf
objektwerks <objektwerks@runbox.com>
2022-12-01 09:39:52 -0500

    rearranged store code

--- server/src/main/scala/objektwerks/Store.scala
+++ server/src/main/scala/objektwerks/Store.scala
@@ -13,13 +13,15 @@ import zio.{Task, ZLayer}
 final case class Store(quill: Quill.Postgres[SnakeCase]):
   import quill.*
 
+  def listAccounts: Task[List[Account]] = run( query[Account] )
+
   def addAccount(account: Account): Task[Long] =
     run( query[Account].insertValue( lift(account) ).returningGenerated(_.id) )
 
   def updateAcount(account: Account): Task[Long] =
     run( query[Account].filter(_.id == lift(account.id) ).updateValue( lift(account) ) )
 
-  def listAccounts: Task[List[Account]] = run( query[Account] )
+  def listPools: Task[List[Pool]] = run( query[Pool] )
 
   def addPool(pool: Pool): Task[Long] =
     run( query[Pool].insertValue( lift(pool) ).returningGenerated(_.id) )
@@ -27,7 +29,7 @@ final case class Store(quill: Quill.Postgres[SnakeCase]):
   def updatePool(pool: Pool): Task[Long] =
     run( query[Pool].filter(_.id == lift(pool.id) ).updateValue( lift(pool) ) )
 
-  def listPools: Task[List[Pool]] = run( query[Pool] )
+  def listCleanings: Task[List[Cleaning]] = run( query[Cleaning] )
 
   def addCleaning(cleaning: Cleaning): Task[Long] =
     run( query[Cleaning].insertValue( lift(cleaning) ).returningGenerated(_.id) )
@@ -35,7 +37,7 @@ final case class Store(quill: Quill.Postgres[SnakeCase]):
   def updateCleaning(cleaning: Cleaning): Task[Long] =
     run( query[Cleaning].filter(_.id == lift(cleaning.id) ).updateValue( lift(cleaning) ) )
 
-  def listCleanings: Task[List[Cleaning]] = run( query[Cleaning] )
+  def listMeasurements: Task[List[Measurement]] = run( query[Measurement] )
 
   def addMeasurement(measurement: Measurement): Task[Long] =
     run( query[Measurement].insertValue( lift(measurement) ).returningGenerated(_.id) )
@@ -43,7 +45,7 @@ final case class Store(quill: Quill.Postgres[SnakeCase]):
   def updateMeasurement(measurement: Measurement): Task[Long] =
     run( query[Measurement].filter(_.id == lift(measurement.id) ).updateValue( lift(measurement) ) )
 
-  def listMeasurements: Task[List[Measurement]] = run( query[Measurement] )
+  def listChemicals: Task[List[Chemical]] = run( query[Chemical] )
 
   def addChemical(chemical: Chemical): Task[Long] =
     run( query[Chemical].insertValue( lift(chemical) ).returningGenerated(_.id) )
@@ -51,17 +53,15 @@ final case class Store(quill: Quill.Postgres[SnakeCase]):
   def updateChemical(chemical: Chemical): Task[Long] =
     run( query[Chemical].filter(_.id == lift(chemical.id) ).updateValue( lift(chemical) ) )
 
-  def listChemicals: Task[List[Chemical]] = run( query[Chemical] )
+  def listEmails: Task[List[Email]] = run( query[Email] )
 
   def addEmail(email: Email): Task[Long] =
     run( query[Email].insertValue( lift(email) ).returningGenerated(_.id) )
 
-  def listEmails: Task[List[Email]] = run( query[Email] )
+  def listFaults: Task[List[Fault]] = run( query[Fault] )
 
   def addFault(fault: Fault): Task[Long] =
     run( query[Fault].insertValue( lift(fault) ) )
-
-  def listFaults: Task[List[Fault]] = run( query[Fault] )
 
 object Store:
   def namingStrategy: ZLayer[DataSource, Nothing, Postgres[SnakeCase]] = Quill.Postgres.fromNamingStrategy(SnakeCase)
