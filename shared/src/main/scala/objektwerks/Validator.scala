6ef569a7195a2ab911d779e0605d1ced5f0888cb
objektwerks <objektwerks@runbox.com>
2022-12-01 13:37:55 -0500

    enhanced validator

--- shared/src/main/scala/objektwerks/Validator.scala
+++ shared/src/main/scala/objektwerks/Validator.scala
@@ -5,4 +5,3 @@ object Validator:
     def isLicense: Boolean = if value.nonEmpty then value.length == 36 else false
     def isEmailAddress: Boolean = value.nonEmpty && value.length >= 3 && value.contains("@")
     def isPin: Boolean = value.length == 7
-    def isName: Boolean = value.length >= 2
