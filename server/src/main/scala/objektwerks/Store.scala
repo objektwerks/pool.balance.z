fb6faed50ceb6659049c36c79e46f3eb37007f52
objektwerks <objektwerks@runbox.com>
2022-12-01 04:29:52 -0500

    simplified store

--- server/src/main/scala/objektwerks/Store.scala
+++ server/src/main/scala/objektwerks/Store.scala
@@ -1,50 +1,45 @@
 package objektwerks
 
-import com.typesafe.config.Config
-
 import io.getquill.*
 import io.getquill.jdbczio.Quill
 import io.getquill.jdbczio.Quill.Postgres
 
-import java.io.IOException
-import java.sql.SQLException
-
-import zio.{ZIO, ZLayer}
+import zio.{Task, ZLayer}
 
 final case class Store(quill: Quill.Postgres[SnakeCase]):
   import quill.*
 
-  inline def addPool(pool: Pool): ZIO[Any, SQLException, Long] =
+  inline def addPool(pool: Pool): Task[Long] =
     run( query[Pool].insertValue( lift(pool) ).returningGenerated(_.id) )
 
-  inline def updatePool(pool: Pool): ZIO[Any, SQLException, Long] =
+  inline def updatePool(pool: Pool): Task[Long] =
     run( query[Pool].filter(_.id == lift(pool.id) ).updateValue( lift(pool) ) )
 
-  inline def listPools: ZIO[Any, SQLException, List[Pool]] = run( query[Pool] )
+  inline def listPools: Task[List[Pool]] = run( query[Pool] )
 
-  inline def addCleaning(cleaning: Cleaning): ZIO[Any, SQLException, Long] =
+  inline def addCleaning(cleaning: Cleaning): Task[Long] =
     run( query[Cleaning].insertValue( lift(cleaning) ).returningGenerated(_.id) )
 
-  inline def updateCleaning(cleaning: Cleaning): ZIO[Any, SQLException, Long] =
+  inline def updateCleaning(cleaning: Cleaning): Task[Long] =
     run( query[Cleaning].filter(_.id == lift(cleaning.id) ).updateValue( lift(cleaning) ) )
 
-  inline def listCleanings: ZIO[Any, SQLException, List[Cleaning]] = run( query[Cleaning] )
+  inline def listCleanings: Task[List[Cleaning]] = run( query[Cleaning] )
 
-  inline def addMeasurement(measurement: Measurement): ZIO[Any, SQLException, Long] =
+  inline def addMeasurement(measurement: Measurement): Task[Long] =
     run( query[Measurement].insertValue( lift(measurement) ).returningGenerated(_.id) )
 
-  inline def updateMeasurement(measurement: Measurement): ZIO[Any, SQLException, Long] =
+  inline def updateMeasurement(measurement: Measurement): Task[Long] =
     run( query[Measurement].filter(_.id == lift(measurement.id) ).updateValue( lift(measurement) ) )
 
-  inline def listMeasurements: ZIO[Any, SQLException, List[Measurement]] = run( query[Measurement] )
+  inline def listMeasurements: Task[List[Measurement]] = run( query[Measurement] )
 
-  inline def addChemical(chemical: Chemical): ZIO[Any, SQLException, Long] =
+  inline def addChemical(chemical: Chemical): Task[Long] =
     run( query[Chemical].insertValue( lift(chemical) ).returningGenerated(_.id) )
 
-  inline def updateChemical(chemical: Chemical): ZIO[Any, SQLException, Long] =
+  inline def updateChemical(chemical: Chemical): Task[Long] =
     run( query[Chemical].filter(_.id == lift(chemical.id) ).updateValue( lift(chemical) ) )
 
-  inline def listChemicals: ZIO[Any, SQLException, List[Chemical]] = run( query[Chemical] )
+  inline def listChemicals: Task[List[Chemical]] = run( query[Chemical] )
 
 object Store:
   val layer: ZLayer[Postgres[SnakeCase], Nothing, Store] = ZLayer.fromFunction(apply(_))
