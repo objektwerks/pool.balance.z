541579629bb835135896ce0cf8ceef29bf3255de
objektwerks <objektwerks@runbox.com>
2022-12-01 13:59:57 -0500

    enhanced command

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -2,9 +2,14 @@ package objektwerks
 
 sealed trait Command
 
-final case class Register(emailAddress: String) extends Command
+sealed trait License:
+  val license: String
 
+final case class Register(emailAddress: String) extends Command
 final case class Login(emailAddress: String, pin: String) extends Command
+
+final case class Deactivate(license: String) extends Command with License
+final case class Reactivate(license: String) extends Command with License
 
 final case class ListPools() extends Command
 final case class SavePool(pool: Pool) extends Command
