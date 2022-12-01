a86d40b083debda7aa6192fa4f1e24c4d13ae003
objektwerks <objektwerks@runbox.com>
2022-12-01 08:58:25 -0500

    enhanced handler

--- server/src/main/scala/objektwerks/Handler.scala
+++ server/src/main/scala/objektwerks/Handler.scala
@@ -15,23 +15,27 @@ final case class Handler(authorizer: Authorizer,
         case c @ SavePool(pool)               => savePool(c)
         case c @ ListCleanings()              => listCleanings(c)
         case c @ SaveCleaning(cleaning)       => saveCleaning(c)
-        case c @ ListMeasurements()           => listMeasurements(charArrayOps())
+        case c @ ListMeasurements()           => listMeasurements(c)
         case c @ SaveMeasurement(measurement) => saveMeasurement(c)
-        case c @ ListChemicals()              => ChemicalsListed(Nil)
-        case c @ SaveChemical(chemical)       => ChemicalSaved(0L)
+        case c @ ListChemicals()              => listChemicals(c)
+        case c @ SaveChemical(chemical)       => saveChemical(c)
     )
 
   def listPools(command: ListPools): PoolsListed = PoolsListed(Nil)
 
   def savePool(command: SavePool): PoolSaved = PoolSaved(0L)
 
-  def listCleanings(command: ListChemicals): CleaningsListed = CleaningsListed(Nil)
+  def listCleanings(command: ListCleanings): CleaningsListed = CleaningsListed(Nil)
 
   def saveCleaning(command: SaveCleaning): CleaningSaved = CleaningSaved(0L)
 
   def listMeasurements(command: ListMeasurements): MeasurementsListed = MeasurementsListed(Nil)
 
   def saveMeasurement(command: SaveMeasurement): MeasurementSaved = MeasurementSaved(0L)
+
+  def listChemicals(command: ListChemicals): ChemicalsListed = ChemicalsListed(Nil)
+
+  def saveChemical(command: SaveChemical): ChemicalSaved = ChemicalSaved(0L)
 
 object Handler:
   val layer: ZLayer[Authorizer & Validator & Store, Nothing, Handler] =
