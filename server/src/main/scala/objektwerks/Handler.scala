1b9b9d31208d3ebd11202722ee2b0b6a88cdb06c
objektwerks <objektwerks@runbox.com>
2022-12-01 09:06:25 -0500

    enhanced handler

--- server/src/main/scala/objektwerks/Handler.scala
+++ server/src/main/scala/objektwerks/Handler.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import zio.{UIO, ZIO, ZLayer}
+import zio.{Task, ZIO, ZLayer}
 import zio.json.{DecoderOps, EncoderOps}
 
 import Serializer.given
@@ -8,34 +8,35 @@ import Serializer.given
 final case class Handler(authorizer: Authorizer,
                          validator: Validator,
                          store: Store):
-  def handle(command: Command): UIO[Event] =
-    ZIO.succeed(
-      command match  // TODO! Handler > Authorizer > Validator > Handler > Store
-        case c @ ListPools()                  => listPools(c)
-        case c @ SavePool(pool)               => savePool(c)
-        case c @ ListCleanings()              => listCleanings(c)
-        case c @ SaveCleaning(cleaning)       => saveCleaning(c)
-        case c @ ListMeasurements()           => listMeasurements(c)
-        case c @ SaveMeasurement(measurement) => saveMeasurement(c)
-        case c @ ListChemicals()              => listChemicals(c)
-        case c @ SaveChemical(chemical)       => saveChemical(c)
-    )
+  def handle(command: Command): Task[Event] =
+    command match  // TODO! Handler > Authorizer > Validator > Handler > Store
+      case c @ ListPools()                  => listPools(c)
+      case c @ SavePool(pool)               => savePool(c)
+      case c @ ListCleanings()              => listCleanings(c)
+      case c @ SaveCleaning(cleaning)       => saveCleaning(c)
+      case c @ ListMeasurements()           => listMeasurements(c)
+      case c @ SaveMeasurement(measurement) => saveMeasurement(c)
+      case c @ ListChemicals()              => listChemicals(c)
+      case c @ SaveChemical(chemical)       => saveChemical(c)
 
-  def listPools(command: ListPools): PoolsListed = PoolsListed(Nil)
+  def listPools(command: ListPools): Task[Event] =
+    for
+      pools <- store.listPools
+    yield PoolsListed(pools)
 
-  def savePool(command: SavePool): PoolSaved = PoolSaved(0L)
+  def savePool(command: SavePool): Task[Event] = PoolSaved(0L)
 
-  def listCleanings(command: ListCleanings): CleaningsListed = CleaningsListed(Nil)
+  def listCleanings(command: ListCleanings): Task[Event] = CleaningsListed(Nil)
 
-  def saveCleaning(command: SaveCleaning): CleaningSaved = CleaningSaved(0L)
+  def saveCleaning(command: SaveCleaning): Task[Event] = CleaningSaved(0L)
 
-  def listMeasurements(command: ListMeasurements): MeasurementsListed = MeasurementsListed(Nil)
+  def listMeasurements(command: ListMeasurements): Task[Event] = MeasurementsListed(Nil)
 
-  def saveMeasurement(command: SaveMeasurement): MeasurementSaved = MeasurementSaved(0L)
+  def saveMeasurement(command: SaveMeasurement): Task[Event] = MeasurementSaved(0L)
 
-  def listChemicals(command: ListChemicals): ChemicalsListed = ChemicalsListed(Nil)
+  def listChemicals(command: ListChemicals): Task[Event] = ChemicalsListed(Nil)
 
-  def saveChemical(command: SaveChemical): ChemicalSaved = ChemicalSaved(0L)
+  def saveChemical(command: SaveChemical): Task[Event] = ChemicalSaved(0L)
 
 object Handler:
   val layer: ZLayer[Authorizer & Validator & Store, Nothing, Handler] =
