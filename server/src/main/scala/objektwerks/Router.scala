3bd3613a947e6d4cef7c63db1d4897c0f01c15fa
objektwerks <objektwerks@runbox.com>
2022-12-06 14:45:19 -0500

    added router

---
+++ server/src/main/scala/objektwerks/Router.scala
@@ -0,0 +1,28 @@
+package objektwerks
+
+import zio.ZIO
+import zio.http.{!!, /, ->, Http, Request, Response}
+import zio.http.model.Method
+import zio.json.{DecoderOps, EncoderOps}
+
+import Serializer.given
+
+object Router:
+  val router: Http[Handler, Throwable, Request, Response] = Http.collectZIO[Request] {
+    case request @ Method.POST -> !! / "command" => request.body.asString.flatMap { json =>
+      json.fromJson[Command] match
+        case Right(command) =>
+          for
+            handler <- ZIO.service[Handler]
+            event   <- handler
+                         .handle(command)
+                         .catchAll(throwable =>
+                            val message = s"Error: ${throwable.getMessage}; processing: $command"
+                            ZIO.log(message) zipRight ZIO.succeed(Fault(message))
+                          )
+          yield
+            Response.json( event.toJson )
+        case Left(error) =>
+          ZIO.succeed( Response.json( Fault(error).toJson ) )
+    }
+  }
