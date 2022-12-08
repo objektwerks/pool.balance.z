8f78ce278df44843ddac9361e81d839a972f6fe8
objektwerks <objektwerks@runbox.com>
2022-12-08 17:06:55 -0500

    refactored server test login

--- server/src/test/scala/objektwerks/ServerTest.scala
+++ server/src/test/scala/objektwerks/ServerTest.scala
@@ -54,13 +54,8 @@ object ServerTest extends ZIOSpecDefault:
     for
       response <- Client.request(url = url, content = Body.fromString(Login(account.pin).toJson))
       result   <- response.body.asString.flatMap { json =>
-                    json.fromJson[Event] match
-                      case Right(event) => event match
-                        case LoggedIn(account) =>
-                          assertTrue(account.isActivated)
-                        case _ =>
-                          Console.printLine("Login > LoggedIn failed!") *> assertTrue(false)
-                      case Left(error) =>
-                        Console.printLine(s"Login > LoggedIn failed: $error") *> assertTrue(false)
+                    json.fromJson[LoggedIn] match
+                      case Right(loggedIn) => assertTrue(loggedIn.account.isActivated)
+                      case Left(error) => Console.printLine(s"Login > LoggedIn failed: $error") *> assertTrue(false)
                     }
     yield result
