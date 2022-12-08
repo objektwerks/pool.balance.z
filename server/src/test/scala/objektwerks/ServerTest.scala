87f8fe92b3934a7224d97ad583f1481da8080aec
objektwerks <objektwerks@runbox.com>
2022-12-08 14:06:26 -0500

    enhanced server test

--- server/src/test/scala/objektwerks/ServerTest.scala
+++ server/src/test/scala/objektwerks/ServerTest.scala
@@ -2,19 +2,30 @@ package objektwerks
 
 import scala.sys.process.Process
 
-import zio.{Console, ZIO}
-import zio.http.Client
+import zio.{Console, Scope, ZIO}
+import zio.http.{Client, ClientConfig}
+import zio.http.netty.client.ConnectionPool
 import zio.json.{DecoderOps, EncoderOps}
-import zio.test.{assertTrue, ZIOSpecDefault}
+import zio.test.{assertTrue, assertZIO, ZIOSpecDefault}
+import zio.test.Assertion.isSuccess
 
 import Serializer.given
 import Validator.given
 import Validator.*
 
+
 object ServerTest extends ZIOSpecDefault:
   val exitCode = Process("psql -d poolbalance -f ddl.sql").run().exitValue()
   val server = Server.run
   val url = "http://localhost:7272/command"
+
+  def spec = suite("server")(
+    test("run") {
+      for
+        result <- register
+      yield assertTrue(result.isSuccess)
+    }
+  ).provide(Client.default, Scope.default)
 
   val register =
     for {
@@ -26,9 +37,3 @@ object ServerTest extends ZIOSpecDefault:
                     case Left(error) => Console.printLine(s"Registered failed: $error") *> assertTrue(false)
                   }
     } yield result
-
-  def spec = suite("server")(
-    test("run") {
-      assertTrue(true)
-    }
-  )
