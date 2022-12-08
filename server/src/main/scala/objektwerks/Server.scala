95b2f9934f675222177c42a0bd53e95284674237
objektwerks <objektwerks@runbox.com>
2022-12-08 11:58:35 -0500

    refactored server

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -2,7 +2,7 @@ package objektwerks
 
 import java.nio.file.Path
 
-import zio.{Runtime, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 import zio.http.{Server => HttpServer, ServerConfig}
 import zio.logging.{LogFormat, file}
 
@@ -12,21 +12,20 @@ object Server extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> file( Path.of("~/.poolbalance.z/server.log") )
 
-  override def run: ZIO[Any, Throwable, HttpServer] =
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] =
     for
       config <- Resources.loadZIOConfig("server.conf")
       host   =  config.getString("host")
       port   =  config.getInt("port")
       _      <- ZIO.log(s"*** Server running at http://$host:$port")
-      server <- HttpServer
-                  .serve(Router.router)
-                  .provide(
-                    ServerConfig.live( ServerConfig.default.port(port) ),
-                    HttpServer.live,
-                    Handler.layer,
-                    Store.layer,
-                    Store.dataSourceLayer,
-                    Store.namingStrategyLayer,
-                    Store.licenseCacheLayer
-                  )
-    yield server
+    yield HttpServer
+            .serve(Router.router)
+            .provide(
+              ServerConfig.live( ServerConfig.default.binding(host, port) ),
+              HttpServer.live,
+              Handler.layer,
+              Store.layer,
+              Store.dataSourceLayer,
+              Store.namingStrategyLayer,
+              Store.licenseCacheLayer
+            )
