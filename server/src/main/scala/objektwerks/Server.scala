0c5a65b80e7784268fa8c9fe6568d8b1a18cc96b
objektwerks <objektwerks@runbox.com>
2022-12-08 12:46:36 -0500

    refactored server and server test

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -3,7 +3,7 @@ package objektwerks
 import java.nio.file.Path
 
 import zio.{Runtime, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
-import zio.http.{Server => HttpServer, ServerConfig}
+import zio.http.ServerConfig
 import zio.logging.{LogFormat, file}
 
 import Serializer.given
@@ -12,17 +12,17 @@ object Server extends ZIOAppDefault:
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Environment] =
     Runtime.removeDefaultLoggers >>> file( Path.of("~/.poolbalance.z/server.log") )
 
-  override def run: ZIO[Environment & (ZIOAppArgs & Scope), Any, Any] =
+  override def run =
     for
       config <- Resources.loadZIOConfig("server.conf")
       host   =  config.getString("host")
       port   =  config.getInt("port")
       _      <- ZIO.log(s"*** Server running at http://$host:$port")
-    yield HttpServer
+    yield zio.http.Server
             .serve(Router.router)
             .provide(
               ServerConfig.live( ServerConfig.default.binding(host, port) ),
-              HttpServer.live,
+              zio.http.Server.live,
               Handler.layer,
               Store.layer,
               Store.dataSourceLayer,
--- server/src/test/scala/objektwerks/ServerTest.scala
+++ server/src/test/scala/objektwerks/ServerTest.scala
@@ -3,15 +3,14 @@ package objektwerks
 import scala.sys.process.Process
 
 import zio.ZIO
-import zio.http.{!!, /, ->, Body, Http, Request, Response, URL}
-import zio.http.model.Method
 import zio.test.{assertTrue, ZIOSpecDefault}
 
 object ServerTest extends ZIOSpecDefault:
   val exitCode = Process("psql -d poolmate -f ddl.sql").run().exitValue()
-
+  
   def spec = suite("server")(
     test("run") {
+      val server = Server.run
       assertTrue(true)
     }
   )
