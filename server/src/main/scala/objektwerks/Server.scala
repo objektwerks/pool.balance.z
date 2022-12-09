b7fdf78a26931e60a0c67f4582f9c57aad1ce369
objektwerks <objektwerks@runbox.com>
2022-12-09 07:46:37 -0500

    refactored server run

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -19,21 +19,22 @@ object Server extends ZIOAppDefault:
     if !Files.exists(serverPath) then serverPath = Files.createDirectory(serverPath)
     Runtime.removeDefaultLoggers >>> file( Path.of(s"$serverDir/server.log") )
 
-  override def run =
+  override def run: ZIO[Environment & (ZIOAppArgs & Scope ), Any, Any] =
     for
       config <- Resources.loadZIOConfig("server.conf")
       host   =  config.getString("host")
       port   =  config.getInt("port")
       _      <- ZIO.log(s"*** Server running at http://$host:$port")
-    yield zio.http.Server
-            .serve(Router.router)
-            .provide(
-              ServerConfig.live( ServerConfig.default.binding(host, port) ),
-              zio.http.Server.live,
-              Handler.layer,
-              Store.layer,
-              Store.dataSourceLayer,
-              Store.namingStrategyLayer,
-              Store.licenseCacheLayer
-            )
-            .exitCode
+      server <- zio.http.Server
+                  .serve(Router.router)
+                  .provide(
+                    ServerConfig.live( ServerConfig.default.binding(host, port) ),
+                    zio.http.Server.live,
+                    Handler.layer,
+                    Store.layer,
+                    Store.dataSourceLayer,
+                    Store.namingStrategyLayer,
+                    Store.licenseCacheLayer
+                  )
+                  .exitCode
+    yield server
