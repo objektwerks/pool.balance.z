faccbe39197e235bd0767a5417e0e2efbb30cc9a
objektwerks <objektwerks@runbox.com>
2022-12-16 08:30:29 -0500

    added logging to client

--- client/src/main/scala/objektwerks/Client.scala
+++ client/src/main/scala/objektwerks/Client.scala
@@ -1,13 +1,15 @@
 package objektwerks
 
-import java.awt.EventQueue
+import com.typesafe.scalalogging.LazyLogging
 
+import java.awt.EventQueue
 import javax.swing.UIManager._
 
-object Client:
+object Client extends LazyLogging:
   def main(args: Array[String]): Unit =
     EventQueue.invokeLater( () => {
       setLookAndFeel(getCrossPlatformLookAndFeelClassName)
       val frame = new Frame("Pool Balance", 600, 600)
       frame.setVisible(true)
+      logger.info("Started client!")
     })
