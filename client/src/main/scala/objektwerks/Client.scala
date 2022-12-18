c616916b20e0f8aa0b0999ce2285746f9aaf3def
objektwerks <objektwerks@runbox.com>
2022-12-18 12:59:48 -0500

    added conf to client

--- client/src/main/scala/objektwerks/Client.scala
+++ client/src/main/scala/objektwerks/Client.scala
@@ -9,7 +9,9 @@ object Client extends LazyLogging:
   def main(args: Array[String]): Unit =
     EventQueue.invokeLater( () => {
       UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-      val frame = new Frame("Pool Balance", 600, 600)
+      val conf = Resources.loadConfig("client.conf")
+      val name = conf.getString("name")
+      val frame = new Frame(name, 600, 600)
       frame.setVisible(true)
       logger.info("Client running!")
     } )
