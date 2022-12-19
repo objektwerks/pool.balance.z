f347f78ad783335a3ed8d52bdb2a8f49a72f19a9
objektwerks <objektwerks@runbox.com>
2022-12-19 09:42:30 -0500

    refactored proxy

--- client/src/main/scala/objektwerks/Proxy.scala
+++ client/src/main/scala/objektwerks/Proxy.scala
@@ -5,7 +5,7 @@ import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-import zio.ZIO
+import zio.{Unsafe, Runtime, ZIO}
 import zio.http.{Body, Client}
 import zio.json.{DecoderOps, EncoderOps}
 
@@ -13,23 +13,35 @@ import Context.*
 import Serializer.given
 
 object Proxy extends LazyLogging:
+  def delegate(command: Command,
+               handler: Event => Unit): ZIO[Any, Nothing, ZIO[Client, Throwable, Unit]] =
+    ZIO.succeed(
+      for
+        _        <- ZIO.succeed( logger.info(s"*** Proxy command: $command") )
+        response <- Client.request(
+                      url = url,
+                      headers = headers,
+                      content = Body.fromString(command.toJson)
+                    )
+        _        <- response.body.asString.flatMap { json =>
+                      json.fromJson[Event] match
+                        case Right(event) =>
+                          ZIO.succeed( logger.info(s"*** Proxy event: $event") ) *>
+                          ZIO.succeed( handler(event) ).unit
+                        case Left(error) =>
+                          val fault = Fault(error)
+                          ZIO.succeed( logger.info(s"*** Proxy fault: $fault") ) *>
+                          ZIO.succeed( handler(fault) ).unit
+                    }
+      yield ()
+    )
+
   def call(command: Command,
            handler: Event => Unit): Unit =
-    for
-      _        <- ZIO.succeed( logger.info(s"*** Proxy command: $command") )
-      response <- Client.request(
-                    url = url,
-                    headers = headers,
-                    content = Body.fromString(command.toJson)
-                  )
-      _        <- response.body.asString.flatMap { json =>
-                    json.fromJson[Event] match
-                      case Right(event) =>
-                        ZIO.succeed( logger.info(s"*** Proxy event: $event") ) *>
-                        ZIO.succeed( handler(event) ).unit
-                      case Left(error) =>
-                        val fault = Fault(error)
-                        ZIO.succeed( logger.info(s"*** Proxy fault: $fault") ) *>
-                        ZIO.succeed( handler(fault) ).unit
-                  }
-    yield ()
+    Unsafe.unsafe { implicit unsafe =>
+      Runtime
+        .default
+        .unsafe
+        .run( delegate(command, handler) )
+        .getOrThrowFiberFailure()
+  }
