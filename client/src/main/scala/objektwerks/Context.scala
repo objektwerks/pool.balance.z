c83c2fd02c60e8378f54e56d81254322648425d7
objektwerks <objektwerks@runbox.com>
2022-12-19 08:00:03 -0500

    enhanced context

--- client/src/main/scala/objektwerks/Context.scala
+++ client/src/main/scala/objektwerks/Context.scala
@@ -4,6 +4,10 @@ object Context:
   val conf = Resources.loadConfig("client.conf")
 
   val name = conf.getString("name")
-  
+
   val width = conf.getInt("width")
   val height = conf.getInt("height")
+
+  val host = conf.getString("host")
+  val port = conf.getInt("port")
+  val url = s"http://$host:$port/command"
