d9be7bfb12e55f818b6dc721a70950fc817914d8
objektwerks <objektwerks@runbox.com>
2022-12-21 02:49:03 -0500

    enhanced form

--- client/src/main/scala/objektwerks/Form.scala
+++ client/src/main/scala/objektwerks/Form.scala
@@ -1,18 +1,20 @@
 package objektwerks
 
 import java.awt.GridLayout
-import javax.swing.{JComponent, JLabel, JPanel}
+import javax.swing.{BorderFactory, JComponent, JLabel, JPanel}
 
 object Form:
   val columns = 2
   val horizontalGap = 6
   val verticalGap = 6
+  val inset = 6
 
 class Form(fields: List[(String, JComponent)]) extends JPanel:
   import Form.*
 
   val rows = fields.length
   setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
+  setBorder(BorderFactory.createEmptyBorder(inset, inset, inset, inset))
 
   for ((label, component) <- fields)
     add( new JLabel(label) )
