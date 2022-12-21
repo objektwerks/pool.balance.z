1ac514af639b2b019c549212ab9debc2b17d669c
objektwerks <objektwerks@runbox.com>
2022-12-21 16:22:47 -0500

    enhanced pool table

--- client/src/main/scala/objektwerks/PoolTable.scala
+++ client/src/main/scala/objektwerks/PoolTable.scala
@@ -1,10 +1,17 @@
 package objektwerks
 
 import javax.swing.JTable
-import javax.swing.table.DefaultTableModel
+import javax.swing.table.{ DefaultTableModel, DefaultTableColumnModel, TableColumn }
 
-final class PoolTableModel(pools: Array[Pool]) extends DefaultTableModel:
+final class PoolTableModel(pools: List[Pool]) extends DefaultTableModel:
   addRow( pools.toArray[Object] )
 
-final class PoolTable(pools: List[Pool]) extends JTable:
-  
+final class PoolColumnModel(columns: List[String]) extends DefaultTableColumnModel:
+  for ((column, index) <- columns.view.zipWithIndex)
+    val tableColumn = new TableColumn(index)
+    tableColumn.setHeaderValue(column)
+    addColumn(tableColumn)
+
+final class PoolTable(pools: List[Pool], columns: List[String]) extends JTable:
+  setModel( PoolTableModel(pools) )
+  setColumnModel( PoolColumnModel(columns) )
