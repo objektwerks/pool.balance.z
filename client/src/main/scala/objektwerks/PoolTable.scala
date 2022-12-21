a50b1eaf29b0d3b2cddce3a42a960aa3881472c0
objektwerks <objektwerks@runbox.com>
2022-12-21 15:04:17 -0500

    added table, replacing pool table

--- client/src/main/scala/objektwerks/PoolTable.scala
+++
@@ -1,6 +0,0 @@
-package objektwerks
-
-import javax.swing.JTable
-
-final class PoolTable(pools: List[Pool]) extends JTable:
-  
---
+++ client/src/main/scala/objektwerks/Table.scala
@@ -0,0 +1,6 @@
+package objektwerks
+
+import javax.swing.JTable
+
+final class Table[E](entities: List[E]) extends JTable:
+  
