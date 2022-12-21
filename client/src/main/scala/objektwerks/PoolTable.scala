a86728a623cf38b68633f9410ce573f193701d11
objektwerks <objektwerks@runbox.com>
2022-12-21 15:27:32 -0500

    readded pool table

---
+++ client/src/main/scala/objektwerks/PoolTable.scala
@@ -0,0 +1,6 @@
+package objektwerks
+
+import javax.swing.JTable
+
+final class PoolTable(pools: List[Pool]) extends JTable:
+  
--- client/src/main/scala/objektwerks/Table.scala
+++
@@ -1,7 +0,0 @@
-package objektwerks
-
-import javax.swing.JTable
-
-final class Table[E](entities: List[E],
-                     columns: List[String]) extends JTable:
-  
