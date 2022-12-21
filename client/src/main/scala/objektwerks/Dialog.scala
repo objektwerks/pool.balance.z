c89cb40494175fb119ec74a3ed025bcb4cbf005d
objektwerks <objektwerks@runbox.com>
2022-12-21 08:52:33 -0500

    added dialog

---
+++ client/src/main/scala/objektwerks/Dialog.scala
@@ -0,0 +1,19 @@
+package objektwerks
+
+import java.awt.{GridLayout, Image}
+import javax.swing.JDialog
+
+object Dialog:
+  val rows = 2
+  val columns = 1
+  val horizontalGap = 6
+  val verticalGap = 6
+
+final class Dialog(image: Image,
+                   title: String) extends JDialog:
+  import Dialog.*
+
+  setIconImage(image)
+  setTitle(title)
+  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
+  setModal(true)
