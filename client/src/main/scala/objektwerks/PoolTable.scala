c8f4a6ffe31ea78ceb6b8e6f9fa5879533adcad4
objektwerks <objektwerks@runbox.com>
2022-12-21 16:09:35 -0500

    enhanced pool table

--- client/src/main/scala/objektwerks/PoolTable.scala
+++ client/src/main/scala/objektwerks/PoolTable.scala
@@ -1,6 +1,10 @@
 package objektwerks
 
 import javax.swing.JTable
+import javax.swing.table.DefaultTableModel
+
+final class PoolTableModel(pools: Array[Pool]) extends DefaultTableModel:
+  addRow( pools.toArray[Object] )
 
 final class PoolTable(pools: List[Pool]) extends JTable:
   
