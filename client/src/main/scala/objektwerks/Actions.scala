1b70dfd0a884e65d9d970547bbb0aaeec13ec1cb
objektwerks <objektwerks@runbox.com>
2022-12-21 09:02:25 -0500

    enhanced actions

--- client/src/main/scala/objektwerks/Actions.scala
+++ client/src/main/scala/objektwerks/Actions.scala
@@ -1,6 +1,15 @@
 package objektwerks
 
+import java.awt.GridLayout
 import javax.swing.{JButton, JPanel}
 
-class Actions(buttons: JButton*) extends JPanel:
-  
+object Actions:
+  val rows = 1
+  val horizontalGap = 6
+  val verticalGap = 6
+
+final class Actions(buttons: JButton*) extends JPanel:
+  import Actions.*
+
+  val columns = buttons.length
+  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
