08ac2e1a374807854a3f6977f5b0156ccfb7a3eb
objektwerks <objektwerks@runbox.com>
2022-12-22 16:12:00 -0500

    added tabs to tabbed pane

--- client/src/main/scala/objektwerks/pane/TabbedPane.scala
+++ client/src/main/scala/objektwerks/pane/TabbedPane.scala
@@ -3,4 +3,6 @@ package objektwerks.pane
 import javax.swing.JTabbedPane
 
 final class TabbedPane extends JTabbedPane:
-  
+  addTab( "Cleanings", CleaningsPane() )
+  addTab( "Measurements", MeasurementsPane() )
+  addTab( "Chemicals", ChemicalsPane() )
