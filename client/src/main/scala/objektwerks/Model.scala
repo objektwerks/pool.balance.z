86475d025bade5c8d5da5cf6b9bc55fd6ed70583
objektwerks <objektwerks@runbox.com>
2022-12-22 11:46:12 -0500

    enhanced model

--- client/src/main/scala/objektwerks/Model.scala
+++ client/src/main/scala/objektwerks/Model.scala
@@ -5,3 +5,8 @@ object Model:
   val cleanings = List.empty[Cleaning]
   val measurements = List.empty[Measurement]
   val chemicals = List.empty[Chemical]
+
+  val selectedPoolId = 0
+  val selectedCleaningId = 0
+  val selectedMeasurementId = 0
+  val selectedChemicalId = 0
