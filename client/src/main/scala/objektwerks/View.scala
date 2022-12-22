65d2945974b356c2ab550439764e8867c4ebe18a
objektwerks <objektwerks@runbox.com>
2022-12-22 16:35:51 -0500

    refactored view layout

--- client/src/main/scala/objektwerks/View.scala
+++ client/src/main/scala/objektwerks/View.scala
@@ -1,21 +1,12 @@
 package objektwerks
 
-import java.awt.GridLayout
+import java.awt.BorderLayout
 import javax.swing.JPanel
 
 import objektwerks.dashboard.DashboardPane
 import objektwerks.pane.SplitPane
 
-object View:
-  val rows = 2
-  val columns = 1
-  val horizontalGap = 6
-  val verticalGap = 6
-
 final class View extends JPanel:
-  import View.*
-
-  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
-
-  add( DashboardPane() )
-  add( SplitPane() )
+  setLayout( new BorderLayout() )
+  add( DashboardPane(), BorderLayout.NORTH )
+  add( SplitPane(), BorderLayout.CENTER )
