4bccea1939288e04231507337229e1329317c651
objektwerks <objektwerks@runbox.com>
2022-12-22 15:52:55 -0500

    added dashboard pane

---
+++ client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
@@ -0,0 +1,15 @@
+package objektwerks.dashboard
+
+import java.awt.GridLayout
+import javax.swing.JPanel
+
+object DashboardPane:
+  val rows = 1
+  val columns = 9
+  val horizontalGap = 6
+  val verticalGap = 6
+
+final class DashboardPane extends JPanel:
+  import DashboardPane.*
+
+  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
