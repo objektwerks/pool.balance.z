e367177a5a563bb4446d1fc685cfd2c0e1097812
objektwerks <objektwerks@runbox.com>
2022-12-23 07:51:49 -0500

    enhanced dasboard titled pane

--- client/src/main/scala/objektwerks/dashboard/DashboardTitledPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardTitledPane.scala
@@ -4,6 +4,8 @@ import java.awt.GridLayout
 import javax.swing.{JLabel, JPanel}
 import javax.swing.border.TitledBorder
 
+import objektwerks.Form
+
 abstract class DashboardTitledPane(title: String) extends JPanel:
   private val rows = 1
   private val columns = 1
@@ -18,3 +20,13 @@ abstract class DashboardTitledPane(title: String) extends JPanel:
   val ideal = new JLabel("")
   val current = new JLabel("0")
   val average = new JLabel("0")
+
+  val controls = List[(String, JLabel)](
+    "Range:" -> range,
+    "Good:" -> good,
+    "Ideal:" -> ideal,
+    "Current:" -> current,
+    "Average:" -> average
+  )
+  
+  val form = Form(controls)
