6776b61e30054ec725bd384db39dcbb59c0e9a83
objektwerks <objektwerks@runbox.com>
2022-12-23 14:47:04 -0500

    edited ph pane

--- client/src/main/scala/objektwerks/dashboard/PhPane.scala
+++ client/src/main/scala/objektwerks/dashboard/PhPane.scala
@@ -3,9 +3,9 @@ package objektwerks.dashboard
 import objektwerks.Model.*
 
 final class PhPane(title: String = "<html>Ph") extends DashboardTitledPane(title):
-  range.setText("0 - 10")
-  good.setText("1 - 5")
-  ideal.setText(("3"))
+  range.setText("6.2 - 8.4")
+  good.setText("7.2 - 7.6")
+  ideal.setText("7.4")
 
   currentPh.onChange { (_, _, newValue) =>
     current.setText(newValue.toString())
