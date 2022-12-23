c4d42d56063f2f432b7e36dd6dfdf13ee288641f
objektwerks <objektwerks@runbox.com>
2022-12-23 08:15:45 -0500

    implemented total chlorine pane

--- client/src/main/scala/objektwerks/dashboard/TotalChlorinePane.scala
+++ client/src/main/scala/objektwerks/dashboard/TotalChlorinePane.scala
@@ -1,5 +1,18 @@
 package objektwerks.dashboard
 
-final class TotalChlorinePane(title: String = "<html>Total<br>Chlorine")
-  extends DashboardTitledPane(title):
-  
+import objektwerks.Model.*
+
+final class TotalChlorinePane(title: String = "<html>Total<br>Chlorine") extends DashboardTitledPane(title):
+  range.setText("0 - 10")
+  good.setText("1 - 5")
+  ideal.setText(("3"))
+
+  currentTotalChlorine.onChange { (_, _, newValue) =>
+    current.setText(newValue.toString())
+    if totalChlorineInRange(newValue) then inRangeCurrent else outOfRangeCurrent
+  }
+
+  averageTotalChlorine.onChange { (_, _, newValue) =>
+    average.setText(newValue.toString())
+    if totalChlorineInRange(newValue) then inRangeAverage else outOfRangeAverage
+  }
