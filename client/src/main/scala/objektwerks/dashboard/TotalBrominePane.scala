d72ad38f125b14851b143880fc5d6fa304fde943
objektwerks <objektwerks@runbox.com>
2022-12-23 15:14:13 -0500

    added total bromine pane

---
+++ client/src/main/scala/objektwerks/dashboard/TotalBrominePane.scala
@@ -0,0 +1,20 @@
+package objektwerks.dashboard
+
+import objektwerks.Model.*
+
+final class TotalBrominePane(title: String = "<html>Total<br>Bromine") extends DashboardTitledPane(title):
+  range.setText("0 - 20")
+  good.setText("2 - 10")
+  ideal.setText(("5"))
+
+  currentTotalBromine.onChange { (_, _, newValue) =>
+    current.setText(newValue.toString())
+    if isTotalBromineInRange(newValue) then currentIsInRange
+    else currentIsOutOfRange
+  }
+
+  averageTotalBromine.onChange { (_, _, newValue) =>
+    average.setText(newValue.toString())
+    if isTotalBromineInRange(newValue) then averageIsInRange
+    else averageIsOutOfRange
+  }
