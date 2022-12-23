88416eeb5c69eb3409a33e5d5f4a8bf102c6914f
objektwerks <objektwerks@runbox.com>
2022-12-23 07:38:21 -0500

    enhanced dashboard titled pane

--- client/src/main/scala/objektwerks/dashboard/DashboardTitledPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardTitledPane.scala
@@ -1,4 +1,10 @@
 package objektwerks.dashboard
 
+import javax.swing.JLabel
+
 abstract class DashboardTitledPane:
-  
+  val range = new JLabel("")
+  val good = new JLabel("")
+  val ideal = new JLabel("")
+  val current = new JLabel("0")
+  val average = new JLabel("0")
