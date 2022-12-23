5318347d2e1f3aadd7d1caa09f33e57156a2e9d1
objektwerks <objektwerks@runbox.com>
2022-12-23 14:39:31 -0500

    added free chlorine range

---
+++ client/src/main/scala/objektwerks/dashboard/FreeChlorineRange.scala
@@ -0,0 +1,18 @@
+package objektwerks.dashboard
+
+import objektwerks.Model.*
+
+final class FreeChlorineRange(title: String = "<html>Free<br>Chlorine") extends DashboardTitledPane(title):
+  range.setText("0 - 10")
+  good.setText("1 - 5")
+  ideal.setText(("3"))
+
+  currentFreeChlorine.onChange { (_, _, newValue) =>
+    current.setText(newValue.toString())
+    if isTotalChlorineInRange(newValue) then currentIsInRange else currentIsOutOfRange
+  }
+
+  averageFreeChlorine.onChange { (_, _, newValue) =>
+    average.setText(newValue.toString())
+    if isTotalChlorineInRange(newValue) then averageIsInRange else averageIsOutOfRange
+  }
