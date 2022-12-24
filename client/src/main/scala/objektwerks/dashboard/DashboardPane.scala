a30d360b53193a814029c63535d646f9140b1479
objektwerks <objektwerks@runbox.com>
2022-12-24 11:37:25 -0500

    removed dead code from dashboard pane

--- client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
@@ -3,15 +3,7 @@ package objektwerks.dashboard
 import java.awt.GridLayout
 import javax.swing.{BoxLayout, JPanel}
 
-object DashboardPane:
-  val rows = 1
-  val columns = 9
-  val horizontalGap = 6
-  val verticalGap = 6
-
 final class DashboardPane extends JPanel:
-  import DashboardPane.*
-
   setLayout( new BoxLayout(this, BoxLayout.LINE_AXIS) )
 
   add( TotalChlorinePane() )
