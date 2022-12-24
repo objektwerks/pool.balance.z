65b1adf683f51f6b7a6eb2a8208e38013e9631a7
objektwerks <objektwerks@runbox.com>
2022-12-24 11:45:39 -0500

    refactored dashboard pane

--- client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
@@ -4,15 +4,22 @@ import java.awt.GridLayout
 import javax.swing.{BoxLayout, JPanel}
 
 final class DashboardPane extends JPanel:
-  setLayout( new BoxLayout(this, BoxLayout.LINE_AXIS) )
+  val topPanel = new JPanel()
+  topPanel.setLayout( new BoxLayout(topPanel, BoxLayout.LINE_AXIS) )
+  topPanel.add( TotalChlorinePane() )
+  topPanel.add( FreeChlorinePane() )
+  topPanel.add( CombinedChlorinePane() )
+  topPanel.add( PhPane() )
+  topPanel.add( CalciumHardnessPane() )
 
-  add( TotalChlorinePane() )
-  add( FreeChlorinePane() )
-  add( CombinedChlorinePane() )
-  add( PhPane() )
-  add( CalciumHardnessPane() )
-  add( TotalAlkalinityPane() )
-  add( CyanuricAcidPane() )
-  add( TotalBrominePane() )
-  add( SaltPane() )
-  add( TemperaturePane() )
+  val bottomPanel = new JPanel()
+  bottomPanel.setLayout( new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS) )
+  bottomPanel.add( TotalAlkalinityPane() )
+  bottomPanel.add( CyanuricAcidPane() )
+  bottomPanel.add( TotalBrominePane() )
+  bottomPanel.add( SaltPane() )
+  bottomPanel.add( TemperaturePane() )
+
+  setLayout( new BoxLayout(this, BoxLayout.PAGE_AXIS) )
+  add( topPanel )
+  add( bottomPanel )
