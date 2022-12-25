f597ecfc3a22124e8dd2c78695d863d132274dd7
objektwerks <objektwerks@runbox.com>
2022-12-25 10:09:19 -0500

    enhanced add pool action

--- client/src/main/scala/objektwerks/action/AddPoolAction.scala
+++ client/src/main/scala/objektwerks/action/AddPoolAction.scala
@@ -1,7 +1,7 @@
 package objektwerks.action
 
 import java.awt.event.ActionEvent
-import javax.swing.AbstractAction
+import javax.swing.{AbstractAction, ImageIcon}
 
-class AddPoolAction extends AbstractAction:
+class AddPoolAction(name: String, image: ImageIcon) extends AbstractAction(name, image):
   override def actionPerformed(event: ActionEvent): Unit = ???
