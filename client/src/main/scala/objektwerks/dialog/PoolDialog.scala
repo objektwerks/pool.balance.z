6c0a5154304c43069b284e566b81e643cc13c3e7
objektwerks <objektwerks@runbox.com>
2022-12-26 08:45:46 -0500

    addef form to pool dialog

--- client/src/main/scala/objektwerks/dialog/PoolDialog.scala
+++ client/src/main/scala/objektwerks/dialog/PoolDialog.scala
@@ -1,7 +1,20 @@
 package objektwerks.dialog
 
+import javax.swing.{JComboBox, JComponent, JFormattedTextField, JTextField}
+
 import objektwerks.Pool
+import objektwerks.form.Form
 
 final class PoolDialog(title: String = "Pool",
                        pool: Pool) extends Dialog(title):
-  
+  val name = new JTextField()
+  val volume = new JFormattedTextField()
+  val unit = new JComboBox()
+
+  val form = Form(
+    List[(String, JComponent)](
+      "Name:" -> name,
+      "Volume:" -> volume,
+      "Unit:" -> unit
+    )
+  )
