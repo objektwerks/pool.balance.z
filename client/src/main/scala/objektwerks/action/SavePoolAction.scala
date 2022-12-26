b803ee409b229828bcfbf5cae938bb8c79c30afb
objektwerks <objektwerks@runbox.com>
2022-12-26 08:47:13 -0500

    added save pool action

---
+++ client/src/main/scala/objektwerks/action/SavePoolAction.scala
@@ -0,0 +1,7 @@
+package objektwerks.action
+
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+
+final class SavePoolAction extends AbstractAction(name):
+  override def actionPerformed(event: ActionEvent): Unit = ()
