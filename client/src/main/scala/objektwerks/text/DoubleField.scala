f7440b923b8b5f720fff2e50c8c68331abcd8227
objektwerks <objektwerks@runbox.com>
2022-12-26 10:40:27 -0500

    renamed double text field to double field

--- client/src/main/scala/objektwerks/text/DoubleField.scala
+++ client/src/main/scala/objektwerks/text/DoubleField.scala
@@ -3,5 +3,5 @@ package objektwerks.text
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
-final class DoubleTextField(value: Double) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
+final class DoubleField(value: Double) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
   this.setValue(value)
