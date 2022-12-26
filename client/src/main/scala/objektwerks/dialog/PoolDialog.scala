39bedabf30980996083b32b2e4786549e8cf53a7
objektwerks <objektwerks@runbox.com>
2022-12-26 10:53:42 -0500

    edited pool dialog

--- client/src/main/scala/objektwerks/dialog/PoolDialog.scala
+++ client/src/main/scala/objektwerks/dialog/PoolDialog.scala
@@ -1,18 +1,16 @@
 package objektwerks.dialog
 
-import javax.swing.{JComboBox, JComponent, JFormattedTextField, JTextField}
-
-import scala.jdk.CollectionConverters.*
+import javax.swing.JComponent
 
 import objektwerks.{Context, Pool, UnitOfMeasure}
 import objektwerks.action.{Actions, CancelAction, SavePoolAction}
-import objektwerks.field.{IntField, StringField}
+import objektwerks.field.{IntField, SelectField, StringField}
 import objektwerks.form.Form
 
 final class PoolDialog(pool: Pool) extends Dialog(Context.pool):
   val name = new StringField( pool.name, 24 )
   val volume = IntField( pool.volume )
-  val unit = new JComboBox( UnitOfMeasure.toList.asJava.toArray() )
+  val unit = SelectField( UnitOfMeasure.toList )
 
   val form = Form(
     List[(String, JComponent)](
