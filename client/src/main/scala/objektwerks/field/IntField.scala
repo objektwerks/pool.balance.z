580f0790d06a01ecba283391522ea019424873ef
objektwerks <objektwerks@runbox.com>
2022-12-26 11:01:02 -0500

    added columns to int field

--- client/src/main/scala/objektwerks/field/IntField.scala
+++ client/src/main/scala/objektwerks/field/IntField.scala
@@ -3,5 +3,7 @@ package objektwerks.field
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
-final class IntField(value: Int) extends JFormattedTextField( NumberFormat.getIntegerInstance() ):
-  this.setValue(value)
+final class IntField(value: Int,
+                     columns: Int = 10) extends JFormattedTextField( NumberFormat.getIntegerInstance() ):
+  setValue(value)
+  setColumns(columns)
