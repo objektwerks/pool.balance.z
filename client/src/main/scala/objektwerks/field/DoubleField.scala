6e5f9fd58f500b4baa97699e3402e33665114221
objektwerks <objektwerks@runbox.com>
2022-12-26 11:02:00 -0500

    added columns to double field

--- client/src/main/scala/objektwerks/field/DoubleField.scala
+++ client/src/main/scala/objektwerks/field/DoubleField.scala
@@ -3,5 +3,7 @@ package objektwerks.field
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
-final class DoubleField(value: Double) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
-  this.setValue(value)
+final class DoubleField(value: Double,
+                        columns: Int = 10) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
+  setValue(value)
+  setColumns(columns)
