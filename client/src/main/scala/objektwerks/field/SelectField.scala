cd3d810125bf3de5b8c12e2dea331356abb4089c
objektwerks <objektwerks@runbox.com>
2022-12-26 10:55:08 -0500

    enhanced select field

--- client/src/main/scala/objektwerks/field/SelectField.scala
+++ client/src/main/scala/objektwerks/field/SelectField.scala
@@ -4,5 +4,6 @@ import javax.swing.JComboBox
 
 import scala.jdk.CollectionConverters.*
 
-final class SelectField(values: List[String]) extends JComboBox[Object](values.asJava.toArray()):
-  
+final class SelectField(values: List[String],
+                        selectedIndex: Int = 0) extends JComboBox[Object](values.asJava.toArray()):
+  setSelectedIndex(selectedIndex)
