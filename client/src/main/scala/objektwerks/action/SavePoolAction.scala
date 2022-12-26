2a8723b329914e1f331f9d7a0dbb7ddff73f939d
objektwerks <objektwerks@runbox.com>
2022-12-26 08:47:57 -0500

    corrected save pool action error

--- client/src/main/scala/objektwerks/action/SavePoolAction.scala
+++ client/src/main/scala/objektwerks/action/SavePoolAction.scala
@@ -3,5 +3,5 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
-final class SavePoolAction extends AbstractAction(name):
+final class SavePoolAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = ()
