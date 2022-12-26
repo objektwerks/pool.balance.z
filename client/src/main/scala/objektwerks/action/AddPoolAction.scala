7fa04095235a312108bcd8c5e36d9046792109c0
objektwerks <objektwerks@runbox.com>
2022-12-26 10:03:46 -0500

    implemented add pool action

--- client/src/main/scala/objektwerks/action/AddPoolAction.scala
+++ client/src/main/scala/objektwerks/action/AddPoolAction.scala
@@ -3,5 +3,9 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
+import objektwerks.Context
+import objektwerks.dialog.PoolDialog
+import objektwerks.Pool
+
 final class AddPoolAction(name: String) extends AbstractAction(name):
-  override def actionPerformed(event: ActionEvent): Unit = ()
+  override def actionPerformed(event: ActionEvent): Unit = PoolDialog(Context.pool, Pool()).view(true)
