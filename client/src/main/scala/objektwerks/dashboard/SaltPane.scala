70d3880843e858459f01330b96024e0584ff3425
objektwerks <objektwerks@runbox.com>
2022-12-27 06:26:44 -0500

    edited SaltPane

--- client/src/main/scala/objektwerks/dashboard/SaltPane.scala
+++ client/src/main/scala/objektwerks/dashboard/SaltPane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class SaltPane(title: String = "<html>Salt") extends DashboardTitledPane(title):
   range.setText("0 - 3600")
   good.setText("2700 - 3400")
-  ideal.setText(("3200"))
+  ideal.setText("3200")
 
   currentSalt.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isSaltInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isSaltInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageSalt.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isSaltInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isSaltInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
