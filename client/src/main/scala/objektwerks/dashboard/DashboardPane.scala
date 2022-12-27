573dc1b0c10f6113005153da3e3430658373e614
objektwerks <objektwerks@runbox.com>
2022-12-27 15:15:29 -0500

    refactored dashboard display

--- client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
+++ client/src/main/scala/objektwerks/dashboard/DashboardPane.scala
@@ -3,23 +3,14 @@ package objektwerks.dashboard
 import javax.swing.{BoxLayout, JPanel}
 
 final class DashboardPane extends JPanel:
-  val topPanel = new JPanel()
-  topPanel.setLayout( new BoxLayout(topPanel, BoxLayout.LINE_AXIS) )
-  topPanel.add( TotalChlorinePane() )
-  topPanel.add( FreeChlorinePane() )
-  topPanel.add( CombinedChlorinePane() )
-  topPanel.add( PhPane() )
-  topPanel.add( CalciumHardnessPane() )
-
-  val bottomPanel = new JPanel()
-  bottomPanel.setLayout( new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS) )
-  bottomPanel.add( TotalAlkalinityPane() )
-  bottomPanel.add( CyanuricAcidPane() )
-  bottomPanel.add( TotalBrominePane() )
-  bottomPanel.add( SaltPane() )
-  bottomPanel.add( TemperaturePane() )
-
-  setLayout( new BoxLayout(this, BoxLayout.PAGE_AXIS) )
-
-  add( topPanel )
-  add( bottomPanel )
+  setLayout( new BoxLayout(this, BoxLayout.LINE_AXIS) )
+  add( TotalChlorinePane() )
+  add( FreeChlorinePane() )
+  add( CombinedChlorinePane() )
+  add( PhPane() )
+  add( CalciumHardnessPane() )
+  add( TotalAlkalinityPane() )
+  add( CyanuricAcidPane() )
+  add( TotalBrominePane() )
+  add( SaltPane() )
+  add( TemperaturePane() )
