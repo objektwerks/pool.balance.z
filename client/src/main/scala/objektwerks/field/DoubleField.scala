d293c820837e8f07205a10a9463a9a1162a5def3
objektwerks <objektwerks@runbox.com>
2022-12-27 08:46:33 -0500

    added property change listener to double field

--- client/src/main/scala/objektwerks/field/DoubleField.scala
+++ client/src/main/scala/objektwerks/field/DoubleField.scala
@@ -1,9 +1,18 @@
 package objektwerks.field
 
+import java.beans.PropertyChangeEvent
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
 final class DoubleField(value: Double,
-                        columns: Int = 10) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
+                        columns: Int = 10,
+                        fireChangeAction: Double => Unit) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
   setValue(value)
   setColumns(columns)
+
+  addPropertyChangeListener(
+    (event: PropertyChangeEvent) => {
+      if event.getSource.isInstanceOf[DoubleField] then
+        fireChangeAction( getValue.asInstanceOf[Number].doubleValue )
+    }
+  )
