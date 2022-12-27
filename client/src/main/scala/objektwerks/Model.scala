8cb5852bbab5fab0a58319abf88538b942606d0c
objektwerks <objektwerks@runbox.com>
2022-12-27 13:59:13 -0500

    edited model

--- client/src/main/scala/objektwerks/Model.scala
+++ client/src/main/scala/objektwerks/Model.scala
@@ -75,11 +75,8 @@ object Model extends LazyLogging:
   def isTemperatureInRange(value: Int): Boolean = temperatureRange.contains(value)
 
   def currentPool: Option[Pool] = observablePools.find( pool => pool.id == selectedPoolId.get )
-
   def currentCleaning: Option[Cleaning] = observableCleanings.find( cleaning => cleaning.id == selectedCleaningId.get )
-
   def currentMeasurement: Option[Measurement] = observableMeasurements.find( measurement => measurement.id == selectedMeasurementId.get )
-
   def currentChemical: Option[Chemical] = observableChemicals.find( chemical => chemical.id == selectedChemicalId.get )
 
   def pools(): Unit =
