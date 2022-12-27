ec442c6ceebb8efd86ae84292e89e9f550557ec9
objektwerks <objektwerks@runbox.com>
2022-12-27 06:27:33 -0500

    edited TotalAlkalinityPane

--- client/src/main/scala/objektwerks/dashboard/TotalAlkalinityPane.scala
+++ client/src/main/scala/objektwerks/dashboard/TotalAlkalinityPane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class TotalAlkalinityPane(title: String = "<html>Total Alkalinity") extends DashboardTitledPane(title):
   range.setText("0 - 240")
   good.setText("80 - 120")
-  ideal.setText(("100"))
+  ideal.setText("100")
 
   currentTotalAlkalinity.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isTotalAlkalinityInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isTotalAlkalinityInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageTotalAlkalinity.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isTotalAlkalinityInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isTotalAlkalinityInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
