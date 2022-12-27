3d4a59fa730a8781df474d24d65055713acdcfd2
objektwerks <objektwerks@runbox.com>
2022-12-27 06:25:56 -0500

    edited FreeChlorinePane

--- client/src/main/scala/objektwerks/dashboard/FreeChlorinePane.scala
+++ client/src/main/scala/objektwerks/dashboard/FreeChlorinePane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class FreeChlorinePane(title: String = "<html>Free Chlorine") extends DashboardTitledPane(title):
   range.setText("0 - 10")
   good.setText("1 - 5")
-  ideal.setText(("3"))
+  ideal.setText("3")
 
   currentFreeChlorine.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isFreeChlorineInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isFreeChlorineInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageFreeChlorine.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isFreeChlorineInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isFreeChlorineInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
