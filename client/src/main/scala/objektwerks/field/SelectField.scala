c8f898857b201d5239b78ff19aa41953365bcf4f
objektwerks <objektwerks@runbox.com>
2022-12-27 06:21:29 -0500

    edited select field

--- client/src/main/scala/objektwerks/field/SelectField.scala
+++ client/src/main/scala/objektwerks/field/SelectField.scala
@@ -1,10 +1,11 @@
 package objektwerks.field
 
+import java.util
 import java.util.Vector
 import javax.swing.JComboBox
 
 import scala.jdk.CollectionConverters.*
 
 final class SelectField(values: List[String],
-                        selectedIndex: Int = 0) extends JComboBox[String]( new Vector( values.asJava ) ):
+                        selectedIndex: Int = 0) extends JComboBox[String]( new util.Vector( values.asJava ) ):
   setSelectedIndex(selectedIndex)
