cf9bf7e189214084c42061d472707cdbf4611e8d
objektwerks <objektwerks@runbox.com>
2022-12-27 06:24:13 -0500

    edited CalciumHardnessPane

--- client/src/main/scala/objektwerks/dashboard/CalciumHardnessPane.scala
+++ client/src/main/scala/objektwerks/dashboard/CalciumHardnessPane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class CalciumHardnessPane(title: String = "<html>Calcium Hardness") extends DashboardTitledPane(title):
   range.setText("0 - 1000")
   good.setText("200 - 500")
-  ideal.setText(("375"))
+  ideal.setText("375")
 
   currentCalciumHardness.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isCalciumHardnessInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isCalciumHardnessInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageCalciumHardness.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isCalciumHardnessInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isCalciumHardnessInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
