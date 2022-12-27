74809e42321d56a6be1dab42e24f46653dc5f77c
objektwerks <objektwerks@runbox.com>
2022-12-27 06:24:39 -0500

    edited CombinedChlorinePane

--- client/src/main/scala/objektwerks/dashboard/CombinedChlorinePane.scala
+++ client/src/main/scala/objektwerks/dashboard/CombinedChlorinePane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class CombinedChlorinePane(title: String = "<html>Combined Chlorine") extends DashboardTitledPane(title):
   range.setText("0 - 0.5")
   good.setText("0.1 - 0.2")
-  ideal.setText(("0"))
+  ideal.setText("0")
 
   currentCombinedChlorine.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isCombinedChlorineInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isCombinedChlorineInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageCombinedChlorine.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isCombinedChlorineInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isCombinedChlorineInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
