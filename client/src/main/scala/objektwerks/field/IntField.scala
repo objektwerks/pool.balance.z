dfb0440e61b72071829f0f2ae4faded9731c7b80
objektwerks <objektwerks@runbox.com>
2022-12-27 09:01:14 -0500

    added property change listener in int field

--- client/src/main/scala/objektwerks/field/IntField.scala
+++ client/src/main/scala/objektwerks/field/IntField.scala
@@ -1,9 +1,15 @@
 package objektwerks.field
 
+import java.beans.PropertyChangeEvent
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
 final class IntField(value: Int,
-                     columns: Int = 10) extends JFormattedTextField( NumberFormat.getIntegerInstance() ):
+                     columns: Int = 10,
+                     fireChangeAction: Int => Unit) extends JFormattedTextField( NumberFormat.getIntegerInstance() ):
   setValue(value)
   setColumns(columns)
+
+  addPropertyChangeListener(
+    (_: PropertyChangeEvent) => fireChangeAction(getValue.asInstanceOf[Number].intValue())
+  )
