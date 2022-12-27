ebced132d3107b8d0a8c0fd065d83bea8891104d
objektwerks <objektwerks@runbox.com>
2022-12-27 06:27:11 -0500

    edited TemperaturePane

--- client/src/main/scala/objektwerks/dashboard/TemperaturePane.scala
+++ client/src/main/scala/objektwerks/dashboard/TemperaturePane.scala
@@ -5,16 +5,16 @@ import objektwerks.Model.*
 final class TemperaturePane(title: String = "<html>Temperature") extends DashboardTitledPane(title):
   range.setText("50 - 110")
   good.setText("75 - 85")
-  ideal.setText(("82"))
+  ideal.setText("82")
 
   currentTemperature.onChange { (_, _, newValue) =>
-    current.setText(newValue.toString())
-    if isTemperatureInRange(newValue) then currentIsInRange
-    else currentIsOutOfRange
+    current.setText(newValue.toString)
+    if isTemperatureInRange(newValue) then currentIsInRange()
+    else currentIsOutOfRange()
   }
 
   averageTemperature.onChange { (_, _, newValue) =>
-    average.setText(newValue.toString())
-    if isTemperatureInRange(newValue) then averageIsInRange
-    else averageIsOutOfRange
+    average.setText(newValue.toString)
+    if isTemperatureInRange(newValue) then averageIsInRange()
+    else averageIsOutOfRange()
   }
