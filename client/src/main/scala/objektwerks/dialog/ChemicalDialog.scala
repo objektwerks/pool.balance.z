7e2bb1ecb42a8b346f70623f8dd6e1d0cb5e3513
objektwerks <objektwerks@runbox.com>
2022-12-29 07:48:30 -0500

    added actions to chemical dialog

--- client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
+++ client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
@@ -1,6 +1,11 @@
 package objektwerks.dialog
 
 import objektwerks.{Context, Chemical}
+import objektwerks.action.{Actions, CancelAction, SaveChemicalAction}
 
 final class ChemicalDialog(chemical: Chemical) extends Dialog(Context.chemical):
   var editedChemical = chemical.copy()
+
+  val cancelAction = CancelAction(Context.cancel, () => close())
+  val saveAction = SaveChemicalAction(Context.save, editedChemical)
+  val actions = Actions(cancelAction, saveAction)
