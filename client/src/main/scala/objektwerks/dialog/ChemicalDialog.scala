4b0b7994972427c6f7b9653e56536c3054db7b17
objektwerks <objektwerks@runbox.com>
2022-12-29 08:38:33 -0500

    added field to chemical dialog

--- client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
+++ client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
@@ -2,7 +2,7 @@ package objektwerks.dialog
 
 import javax.swing.{JComponent, JLabel}
 
-import objektwerks.{Context, Chemical, TypeOfChemical}
+import objektwerks.{Context, Chemical, TypeOfChemical, UnitOfMeasure}
 import objektwerks.action.{Actions, CancelAction, SaveChemicalAction}
 import objektwerks.field.{DoubleField, SelectField}
 import objektwerks.form.Form
@@ -12,6 +12,7 @@ final class ChemicalDialog(chemical: Chemical) extends Dialog(Context.chemical):
 
   val typeof = SelectField( TypeOfChemical.toList, (value: String) => editedChemical = chemical.copy(typeof = value) )
   val amount = DoubleField( chemical.amount, (value: Double) => editedChemical = chemical.copy(amount = value) )
+  val unit = SelectField( UnitOfMeasure.toList, (value: String) => editedChemical = chemical.copy(unit = value) )
 
 
   val cancelAction = CancelAction(Context.cancel, () => close())
