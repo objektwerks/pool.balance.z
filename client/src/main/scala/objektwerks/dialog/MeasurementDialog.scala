377f51662941c6a98a29def744d8c03d17c37645
objektwerks <objektwerks@runbox.com>
2022-12-29 07:47:30 -0500

    added actions to measurement dialog

--- client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
+++ client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
@@ -1,6 +1,11 @@
 package objektwerks.dialog
 
 import objektwerks.{Context, Measurement}
+import objektwerks.action.{Actions, CancelAction, SaveMeasurementAction}
 
 final class MeasurementDialog(measurement: Measurement) extends Dialog(Context.measurement):
   var editedMeasurement = measurement.copy()
+
+  val cancelAction = CancelAction(Context.cancel, () => close())
+  val saveAction = SaveMeasurementAction(Context.save, editedMeasurement)
+  val actions = Actions(cancelAction, saveAction)
