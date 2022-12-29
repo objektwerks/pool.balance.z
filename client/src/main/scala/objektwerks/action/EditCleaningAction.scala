26f2f264b59868bf48d2d6b444cc224761fa9a7b
objektwerks <objektwerks@runbox.com>
2022-12-29 07:35:59 -0500

    added editec cleaning action

---
+++ client/src/main/scala/objektwerks/action/EditCleaningAction.scala
@@ -0,0 +1,10 @@
+package objektwerks.action
+
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+
+import objektwerks.dialog.CleaningDialog
+import objektwerks.{Model, Cleaning}
+
+final class EditCleaningAction(name: String) extends AbstractAction(name):
+  override def actionPerformed(event: ActionEvent): Unit = Model.currentCleaning.fold(())(cleaning => CleaningDialog(cleaning).open())
