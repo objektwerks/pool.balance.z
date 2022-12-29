7ea7e73e6a0db5bc949173629bba60c4fef9724e
objektwerks <objektwerks@runbox.com>
2022-12-29 09:02:15 -0500

    refactored select field in chemical dialog

--- client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
+++ client/src/main/scala/objektwerks/dialog/ChemicalDialog.scala
@@ -10,9 +10,17 @@ import objektwerks.form.Form
 final class ChemicalDialog(chemical: Chemical) extends Dialog(Context.chemical):
   var editedChemical = chemical.copy()
 
-  val typeof = SelectField( TypeOfChemical.toList, (value: String) => editedChemical = chemical.copy(typeof = value) )
+  val typeof = SelectField(
+    TypeOfChemical.toList,
+    (value: String) => editedChemical = chemical.copy(typeof = value),
+    TypeOfChemical.selectedIndex(chemical.typeof)
+  )
   val amount = DoubleField( chemical.amount, (value: Double) => editedChemical = chemical.copy(amount = value) )
-  val unit = SelectField( UnitOfMeasure.toList, (value: String) => editedChemical = chemical.copy(unit = value) )
+  val unit = SelectField(
+    UnitOfMeasure.toList,
+    (value: String) => editedChemical = chemical.copy(unit = value),
+    UnitOfMeasure.selectedIndex(UnitOfMeasure.toList, chemical.unit)
+  )
 
   val form = Form(
     List[(String, JComponent)](
