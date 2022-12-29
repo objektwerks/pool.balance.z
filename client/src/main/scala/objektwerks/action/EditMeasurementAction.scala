a067a56a70f9cf7a9247a2cc763a4e3b0b270ed2
objektwerks <objektwerks@runbox.com>
2022-12-29 07:37:37 -0500

    added edit measurement action

---
+++ client/src/main/scala/objektwerks/action/EditMeasurementAction.scala
@@ -0,0 +1,10 @@
+package objektwerks.action
+
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+
+import objektwerks.dialog.MeasurementDialog
+import objektwerks.{Model, Measurement}
+
+final class EditMeasurementAction(name: String) extends AbstractAction(name):
+  override def actionPerformed(event: ActionEvent): Unit = Model.currentMeasurement.fold(())(measurement => MeasurementDialog(measurement).open())
