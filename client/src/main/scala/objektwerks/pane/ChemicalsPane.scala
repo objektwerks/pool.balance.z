d9bea41b95ea1a59e8162d9cbc92172450d7b644
objektwerks <objektwerks@runbox.com>
2022-12-29 17:00:26 -0500

    edited chemicals pane

--- client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
+++ client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
@@ -1,15 +1,6 @@
 package objektwerks.pane
 
-import java.awt.GridLayout
 import javax.swing.JPanel
 
-object ChemicalsPane:
-  val rows = 2
-  val columns = 1
-  val horizontalGap = 6
-  val verticalGap = 6
-
 final class ChemicalsPane extends JPanel:
-  import ChemicalsPane.*
-
-  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
+  
