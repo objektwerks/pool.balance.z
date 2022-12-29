41fa65c69292f162cf7084cdd07bb1a28deae482
objektwerks <objektwerks@runbox.com>
2022-12-29 08:11:44 -0500

    added field to measurement dialog

--- client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
+++ client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
@@ -2,9 +2,14 @@ package objektwerks.dialog
 
 import objektwerks.{Context, Measurement}
 import objektwerks.action.{Actions, CancelAction, SaveMeasurementAction}
+import objektwerks.field.{DoubleField, IntField, StringField}
+import objektwerks.form.Form
 
 final class MeasurementDialog(measurement: Measurement) extends Dialog(Context.measurement):
   var editedMeasurement = measurement.copy()
+
+  val totalChlorine = IntField( measurement.totalChlorine, (value: Int) => editedMeasurement = measurement.copy(totalChlorine = value) )
+
 
   val cancelAction = CancelAction(Context.cancel, () => close())
   val saveAction = SaveMeasurementAction(Context.save, editedMeasurement)
