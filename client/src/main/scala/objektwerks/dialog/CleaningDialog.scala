890d97f67d857590cdfd311b7cc4e15aaca0224b
objektwerks <objektwerks@runbox.com>
2022-12-29 16:55:36 -0500

    edited cleaning dialog

--- client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
+++ client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
@@ -11,12 +11,30 @@ import objektwerks.form.Form
 final class CleaningDialog(cleaning: Cleaning) extends Dialog(Context.cleaning):
   var editedCleaning = cleaning.copy()
 
-  val brush = BooleanField( cleaning.brush, (state: Boolean) => editedCleaning = cleaning.copy(brush = state) )
-  val net = BooleanField( cleaning.net, (state: Boolean) => editedCleaning = cleaning.copy(net = state) )
-  val skimmerBasket = BooleanField( cleaning.skimmerBasket, (state: Boolean) => editedCleaning = cleaning.copy(skimmerBasket = state) )
-  val pumpBasket = BooleanField( cleaning.pumpBasket, (state: Boolean) => editedCleaning = cleaning.copy(pumpBasket = state) )
-  val pumpFilter = BooleanField( cleaning.pumpFilter, (state: Boolean) => editedCleaning = cleaning.copy(pumpFilter = state) )
-  val vacuum = BooleanField( cleaning.vacuum, (state: Boolean) => editedCleaning = cleaning.copy(vacuum = state) )
+  val brush = BooleanField(
+    cleaning.brush,
+    (state: Boolean) => editedCleaning = cleaning.copy(brush = state)
+  )
+  val net = BooleanField(
+    cleaning.net,
+    (state: Boolean) => editedCleaning = cleaning.copy(net = state)
+  )
+  val skimmerBasket = BooleanField(
+    cleaning.skimmerBasket,
+    (state: Boolean) => editedCleaning = cleaning.copy(skimmerBasket = state)
+  )
+  val pumpBasket = BooleanField(
+    cleaning.pumpBasket,
+    (state: Boolean) => editedCleaning = cleaning.copy(pumpBasket = state)
+  )
+  val pumpFilter = BooleanField(
+    cleaning.pumpFilter,
+    (state: Boolean) => editedCleaning = cleaning.copy(pumpFilter = state)
+  )
+  val vacuum = BooleanField(
+    cleaning.vacuum,
+    (state: Boolean) => editedCleaning = cleaning.copy(vacuum = state)
+  )
 
   val form = Form(
     List[(String, JComponent)](
