9eea7ed58b953c6aefa5532c5a0314d78c365608
objektwerks <objektwerks@runbox.com>
2022-12-29 16:58:14 -0500

    edited measurement dialog

--- client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
+++ client/src/main/scala/objektwerks/dialog/MeasurementDialog.scala
@@ -11,16 +11,46 @@ import objektwerks.form.Form
 final class MeasurementDialog(measurement: Measurement) extends Dialog(Context.measurement):
   var editedMeasurement = measurement.copy()
 
-  val totalChlorine = IntField( measurement.totalChlorine, (value: Int) => editedMeasurement = measurement.copy(totalChlorine = value) )
-  val freeChlorine = IntField( measurement.freeChlorine, (value: Int) => editedMeasurement = measurement.copy(freeChlorine = value) )
-  val combinedChlorine = DoubleField( measurement.combinedChlorine, (value: Double) => editedMeasurement = measurement.copy(combinedChlorine = value) )
-  val ph = DoubleField( measurement.ph, (value: Double) => editedMeasurement = measurement.copy(ph = value) )
-  val calciumHardness = IntField( measurement.calciumHardness, (value: Int) => editedMeasurement = measurement.copy(calciumHardness = value) )
-  val totalAlkalinity = IntField( measurement.totalAlkalinity, (value: Int) => editedMeasurement = measurement.copy(totalAlkalinity = value) )
-  val cyanuricAcid = IntField( measurement.cyanuricAcid, (value: Int) => editedMeasurement = measurement.copy(cyanuricAcid = value) )
-  val totalBromine = IntField( measurement.totalBromine, (value: Int) => editedMeasurement = measurement.copy(totalBromine = value) )
-  val salt = IntField( measurement.salt, (value: Int) => editedMeasurement = measurement.copy(salt = value) )
-  val temperature = IntField( measurement.temperature, (value: Int) => editedMeasurement = measurement.copy(temperature = value) )
+  val totalChlorine = IntField(
+    measurement.totalChlorine,
+    (value: Int) => editedMeasurement = measurement.copy(totalChlorine = value)
+  )
+  val freeChlorine = IntField(
+    measurement.freeChlorine,
+    (value: Int) => editedMeasurement = measurement.copy(freeChlorine = value)
+  )
+  val combinedChlorine = DoubleField(
+    measurement.combinedChlorine,
+    (value: Double) => editedMeasurement = measurement.copy(combinedChlorine = value)
+  )
+  val ph = DoubleField(
+    measurement.ph,
+    (value: Double) => editedMeasurement = measurement.copy(ph = value)
+  )
+  val calciumHardness = IntField(
+    measurement.calciumHardness,
+    (value: Int) => editedMeasurement = measurement.copy(calciumHardness = value)
+  )
+  val totalAlkalinity = IntField(
+    measurement.totalAlkalinity,
+    (value: Int) => editedMeasurement = measurement.copy(totalAlkalinity = value)
+  )
+  val cyanuricAcid = IntField(
+    measurement.cyanuricAcid,
+    (value: Int) => editedMeasurement = measurement.copy(cyanuricAcid = value)
+  )
+  val totalBromine = IntField(
+    measurement.totalBromine,
+    (value: Int) => editedMeasurement = measurement.copy(totalBromine = value)
+  )
+  val salt = IntField(
+    measurement.salt,
+    (value: Int) => editedMeasurement = measurement.copy(salt = value)
+  )
+  val temperature = IntField(
+    measurement.temperature,
+    (value: Int) => editedMeasurement = measurement.copy(temperature = value)
+  )
 
   val form = Form(
     List[(String, JComponent)](
