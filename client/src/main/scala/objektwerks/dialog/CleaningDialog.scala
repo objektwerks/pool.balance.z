18f892cc57d3c5ece77e096f3dacfb93367600d4
objektwerks <objektwerks@runbox.com>
2022-12-29 08:06:14 -0500

    added field to cleaning dialog

--- client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
+++ client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
@@ -14,13 +14,15 @@ final class CleaningDialog(cleaning: Cleaning) extends Dialog(Context.cleaning):
   val net = BooleanField( cleaning.net, (state: Boolean) => editedCleaning = cleaning.copy(net = state) )
   val skimmerBasket = BooleanField( cleaning.skimmerBasket, (state: Boolean) => editedCleaning = cleaning.copy(skimmerBasket = state) )
   val pumpBasket = BooleanField( cleaning.pumpBasket, (state: Boolean) => editedCleaning = cleaning.copy(pumpBasket = state) )
+  val pumpFilter = BooleanField( cleaning.pumpFilter, (state: Boolean) => editedCleaning = cleaning.copy(pumpFilter = state) )
 
   val form = Form(
     List[(String, JComponent)](
       "Brush:" -> brush,
       "Net:" -> net,
       "Skimmer Basket:" -> skimmerBasket,
-      "Pump Basket" -> pumpBasket
+      "Pump Basket" -> pumpBasket,
+      "Pump Filter" -> pumpFilter
     )
   )
 
