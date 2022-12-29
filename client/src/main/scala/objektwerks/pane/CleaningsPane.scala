77f0688204b911fdcd360f5737675fcf6e8976bf
objektwerks <objektwerks@runbox.com>
2022-12-29 16:59:28 -0500

    edited cleaning pane

--- client/src/main/scala/objektwerks/pane/CleaningsPane.scala
+++ client/src/main/scala/objektwerks/pane/CleaningsPane.scala
@@ -1,15 +1,6 @@
 package objektwerks.pane
 
-import java.awt.GridLayout
 import javax.swing.JPanel
 
-object CleaningsPane:
-  val rows = 2
-  val columns = 1
-  val horizontalGap = 6
-  val verticalGap = 6
-
 final class CleaningsPane extends JPanel:
-  import CleaningsPane.*
-
-  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
+  
