a2b0d2bb78a8831b83709a1c66f622cec9b7701d
objektwerks <objektwerks@runbox.com>
2022-12-29 08:02:29 -0500

    added field to cleaning dialog

--- client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
+++ client/src/main/scala/objektwerks/dialog/CleaningDialog.scala
@@ -1,10 +1,22 @@
 package objektwerks.dialog
 
+import javax.swing.JComponent
+
 import objektwerks.{Cleaning, Context}
 import objektwerks.action.{Actions, CancelAction, SaveCleaningAction}
+import objektwerks.field.BooleanField
+import objektwerks.form.Form
 
 final class CleaningDialog(cleaning: Cleaning) extends Dialog(Context.cleaning):
   var editedCleaning = cleaning.copy()
+
+  val brush = BooleanField( cleaning.brush, (state: Boolean) => editedCleaning = cleaning.copy(brush = state) )
+
+  val form = Form(
+    List[(String, JComponent)](
+      "Brush:" -> brush
+    )
+  )
 
   val cancelAction = CancelAction(Context.cancel, () => close())
   val saveAction = SaveCleaningAction(Context.save, editedCleaning)
