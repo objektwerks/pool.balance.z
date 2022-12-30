4b48342e0fa3639bc04949fda28df51390d29061
objektwerks <objektwerks@runbox.com>
2022-12-30 03:42:06 -0500

    enhanced measurements pane

--- client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
@@ -3,5 +3,9 @@ package objektwerks.pane
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
+import objektwerks.Model
+
 final class MeasurementsPane extends JPanel:
+  val measurements = Model.observableMeasurements.toList
+
   setLayout( new BorderLayout() )
