bf7701ff2cb32551c1f26323639fca43e8e47d8d
objektwerks <objektwerks@runbox.com>
2022-12-30 03:40:59 -0500

    enhanced cleanings pane

--- client/src/main/scala/objektwerks/pane/CleaningsPane.scala
+++ client/src/main/scala/objektwerks/pane/CleaningsPane.scala
@@ -3,5 +3,9 @@ package objektwerks.pane
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
+import objektwerks.Model
+
 final class CleaningsPane extends JPanel:
+  val cleanings = Model.observableCleanings.toList
+
   setLayout( new BorderLayout() )
