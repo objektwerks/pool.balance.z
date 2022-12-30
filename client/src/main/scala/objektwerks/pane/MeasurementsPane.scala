2ac1896f06a0743515c6ab61179a6f94ba2b5e56
objektwerks <objektwerks@runbox.com>
2022-12-30 03:47:59 -0500

    added columns to measurements pane

--- client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
@@ -4,8 +4,13 @@ import java.awt.BorderLayout
 import javax.swing.JPanel
 
 import objektwerks.Model
+import objektwerks.Context.*
 
 final class MeasurementsPane extends JPanel:
   val measurements = Model.observableMeasurements.toList
+  val columns = List(
+    id, poolId, totalChlorine, freeChlorine, combinedChlorine, ph, calciumHardness,
+    totalAlkalinity, cyanuricAcid, totalBromine, salt, temperature, measured
+  )
 
   setLayout( new BorderLayout() )
