230dee6d9b28b3988760a8bf5db0c6d770925c3c
objektwerks <objektwerks@runbox.com>
2022-12-30 03:48:55 -0500

    added columns to chemicals pane

--- client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
+++ client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
@@ -4,8 +4,10 @@ import java.awt.BorderLayout
 import javax.swing.JPanel
 
 import objektwerks.Model
+import objektwerks.Context.*
 
 final class ChemicalsPane extends JPanel:
   val chemicals = Model.observableChemicals.toList
+  val columns = List(id, poolId, typeof, amount, unit, added)
 
   setLayout( new BorderLayout() )
