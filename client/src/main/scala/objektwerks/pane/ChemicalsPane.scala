5545abde675c6d61ef35e1c798542a6bc7cbddfd
objektwerks <objektwerks@runbox.com>
2022-12-30 03:43:04 -0500

    enhanced chemicals pane

--- client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
+++ client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
@@ -3,5 +3,9 @@ package objektwerks.pane
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
+import objektwerks.Model
+
 final class ChemicalsPane extends JPanel:
+  val chemicals = Model.observableChemicals.toList
+
   setLayout( new BorderLayout() )
