a8efd445176fe2cec86653be0fde02ca15882497
objektwerks <objektwerks@runbox.com>
2022-12-30 08:17:13 -0500

    added input verifier to double range field

--- client/src/main/scala/objektwerks/field/DoubleRangeField.scala
+++ client/src/main/scala/objektwerks/field/DoubleRangeField.scala
@@ -1,14 +1,27 @@
 package objektwerks.field
 
 import java.beans.PropertyChangeEvent
-import javax.swing.JFormattedTextField
 import java.text.NumberFormat
+import javax.swing.{InputVerifier, JComponent, JFormattedTextField}
+import javax.swing.text.JTextComponent
 
 final class DoubleRangeField(value: Double,
                              isInRange: Double => Boolean,
                              fireChangeAction: Double => Unit) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
   setValue(value)
   setColumns(10)
+  setInputVerifier(
+    new InputVerifier {
+      private def isValid(input: JComponent): Boolean =
+        input.asInstanceOf[JTextComponent].getText.toDoubleOption match
+          case Some(value) => isInRange(value)
+          case None => false
+
+      override def verify(input: JComponent): Boolean = isValid(input)
+      override def verifyTarget(input: JComponent): Boolean = isValid(input)
+      override def shouldYieldFocus(source: JComponent, target: JComponent): Boolean = verify(source) || verifyTarget(target)
+    }
+  )
 
   addPropertyChangeListener(
     (_: PropertyChangeEvent) =>
