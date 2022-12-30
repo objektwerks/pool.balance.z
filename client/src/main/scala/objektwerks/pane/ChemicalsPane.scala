0c33582a5aa2ad4fe884a87742371de599855e8a
objektwerks <objektwerks@runbox.com>
2022-12-30 04:01:33 -0500

    implemented chemicals pane

--- client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
+++ client/src/main/scala/objektwerks/pane/ChemicalsPane.scala
@@ -12,5 +12,23 @@ import objektwerks.table.{ColumnModel, Table, TableModel}
 final class ChemicalsPane extends JPanel:
   val chemicals = Model.observableChemicals.toList
   val columns = List(id, poolId, typeof, amount, unit, added)
+  val table = Table(
+    TableModel(chemicals),
+    ColumnModel(columns),
+    Long => setSelectedId(Long),
+    Long => fireEditActionById(Long)
+  )
+  val tablePane = new JScrollPane(table)
+
+  val addAction = AddChemicalAction(Context.add)
+  val editAction = EditChemicalAction(Context.edit)
+  val actions = Actions(addAction, editAction)
+
+  def setSelectedId(id: Long): Unit = Model.selectedChemicalId.value = id
+
+  def fireEditActionById(id: Long): Unit = editAction.actionPerformed( new ActionEvent(table, ActionEvent.ACTION_PERFORMED, id.toString) )
 
   setLayout( new BorderLayout() )
+
+  add(tablePane, BorderLayout.CENTER)
+  add(actions, BorderLayout.SOUTH)
