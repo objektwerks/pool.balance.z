1a2742e22fe7ca38ca78dfd359f62727418dbf90
objektwerks <objektwerks@runbox.com>
2022-12-30 03:58:16 -0500

    added imports to measurements pane

--- client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
@@ -1,10 +1,13 @@
 package objektwerks.pane
 
 import java.awt.BorderLayout
-import javax.swing.JPanel
+import java.awt.event.ActionEvent
+import javax.swing.{JPanel, JScrollPane}
 
-import objektwerks.Model
+import objektwerks.{Context, Model}
 import objektwerks.Context.*
+import objektwerks.action.{Actions, AddMeasurementAction, EditMeasurementAction}
+import objektwerks.table.{ColumnModel, Table, TableModel}
 
 final class MeasurementsPane extends JPanel:
   val measurements = Model.observableMeasurements.toList
