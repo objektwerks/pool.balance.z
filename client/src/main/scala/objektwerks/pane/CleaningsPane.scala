a16153e24d631032f60bfcd11810b06884789249
objektwerks <objektwerks@runbox.com>
2022-12-30 03:45:11 -0500

    added columns to cleanings pane

--- client/src/main/scala/objektwerks/pane/CleaningsPane.scala
+++ client/src/main/scala/objektwerks/pane/CleaningsPane.scala
@@ -4,8 +4,10 @@ import java.awt.BorderLayout
 import javax.swing.JPanel
 
 import objektwerks.Model
+import objektwerks.Context.*
 
 final class CleaningsPane extends JPanel:
   val cleanings = Model.observableCleanings.toList
+  val columns = List(id, poolId, brush, net, skimmerBasket, pumpBasket, pumpFilter, vacuum, cleaned)
 
   setLayout( new BorderLayout() )
