7b725215934acbfccd1cccb2d7dce14c23aca303
objektwerks <objektwerks@runbox.com>
2022-12-30 04:00:25 -0500

    implemented measurements pane

--- client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
+++ client/src/main/scala/objektwerks/pane/MeasurementsPane.scala
@@ -15,5 +15,23 @@ final class MeasurementsPane extends JPanel:
     id, poolId, totalChlorine, freeChlorine, combinedChlorine, ph, calciumHardness,
     totalAlkalinity, cyanuricAcid, totalBromine, salt, temperature, measured
   )
+  val table = Table(
+    TableModel(measurements),
+    ColumnModel(columns),
+    Long => setSelectedId(Long),
+    Long => fireEditActionById(Long)
+  )
+  val tablePane = new JScrollPane(table)
+
+  val addAction = AddMeasurementAction(Context.add)
+  val editAction = EditMeasurementAction(Context.edit)
+  val actions = Actions(addAction, editAction)
+
+  def setSelectedId(id: Long): Unit = Model.selectedCleaningId.value = id
+
+  def fireEditActionById(id: Long): Unit = editAction.actionPerformed( new ActionEvent(table, ActionEvent.ACTION_PERFORMED, id.toString) )
 
   setLayout( new BorderLayout() )
+
+  add(tablePane, BorderLayout.CENTER)
+  add(actions, BorderLayout.SOUTH)
