bc72d2943a828f58ceafc6f8ff9eb1d9d5cdf256
objektwerks <objektwerks@runbox.com>
2022-12-30 03:56:32 -0500

    implemented cleanings pane

--- client/src/main/scala/objektwerks/pane/CleaningsPane.scala
+++ client/src/main/scala/objektwerks/pane/CleaningsPane.scala
@@ -1,13 +1,34 @@
 package objektwerks.pane
 
 import java.awt.BorderLayout
-import javax.swing.JPanel
+import java.awt.event.ActionEvent
+import javax.swing.{JPanel, JScrollPane}
 
-import objektwerks.Model
+import objektwerks.{Context, Model}
 import objektwerks.Context.*
+import objektwerks.action.{Actions, AddCleaningAction, EditCleaningAction}
+import objektwerks.table.{ColumnModel, Table, TableModel}
 
 final class CleaningsPane extends JPanel:
   val cleanings = Model.observableCleanings.toList
   val columns = List(id, poolId, brush, net, skimmerBasket, pumpBasket, pumpFilter, vacuum, cleaned)
+  val table = Table(
+    TableModel(cleanings),
+    ColumnModel(columns),
+    Long => setSelectedId(Long),
+    Long => fireEditActionById(Long)
+  )
+  val tablePane = new JScrollPane(table)
+
+  val addAction = AddCleaningAction(Context.add)
+  val editAction = EditCleaningAction(Context.edit)
+  val actions = Actions(addAction, editAction)
+
+  def setSelectedId(id: Long): Unit = Model.selectedCleaningId.value = id
+
+  def fireEditActionById(id: Long): Unit = editAction.actionPerformed( new ActionEvent(table, ActionEvent.ACTION_PERFORMED, id.toString) )
 
   setLayout( new BorderLayout() )
+
+  add(tablePane, BorderLayout.CENTER)
+  add(actions, BorderLayout.SOUTH)
