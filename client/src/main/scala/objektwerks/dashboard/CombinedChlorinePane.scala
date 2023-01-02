56292177efb0b4c3384177a54009d9ffce61dacf
objektwerks <objektwerks@runbox.com>
2023-01-02 12:09:15 -0500

    refactoring dashboard titled panes

--- client/src/main/scala/objektwerks/dashboard/CombinedChlorinePane.scala
+++ client/src/main/scala/objektwerks/dashboard/CombinedChlorinePane.scala
@@ -1,8 +1,9 @@
 package objektwerks.dashboard
 
+import objektwerks.Context.*
 import objektwerks.Model.*
 
-final class CombinedChlorinePane(title: String = "<html>Combined Chlorine") extends DashboardTitledPane(title):
+final class CombinedChlorinePane(title: String = combinedChlorine.asHtml) extends DashboardTitledPane(title):
   range.setText("0 - 0.5")
   good.setText("0.1 - 0.2")
   ideal.setText("0")
--- client/src/main/scala/objektwerks/dashboard/CyanuricAcidPane.scala
+++ client/src/main/scala/objektwerks/dashboard/CyanuricAcidPane.scala
@@ -1,8 +1,9 @@
 package objektwerks.dashboard
 
+import objektwerks.Context.*
 import objektwerks.Model.*
 
-final class CyanuricAcidPane(title: String = "<html>Cyanuric Acid") extends DashboardTitledPane(title):
+final class CyanuricAcidPane(title: String = cyanuricAcid.asHtml) extends DashboardTitledPane(title):
   range.setText("0 - 300")
   good.setText("30 - 100")
   ideal.setText("50")
