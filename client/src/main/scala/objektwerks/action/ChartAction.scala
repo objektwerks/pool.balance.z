3b294bab61c2598e9d53048efeb5afe9806b3b72
objektwerks <objektwerks@runbox.com>
2023-01-02 11:45:41 -0500

    added chart action

---
+++ client/src/main/scala/objektwerks/action/ChartAction.scala
@@ -0,0 +1,12 @@
+package objektwerks.action
+
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+
+import org.jfree.chart.ChartPanel
+
+import objektwerks.dialog.ChartDialog
+
+final class ChartAction(name: String,
+                        panel: => ChartPanel) extends AbstractAction(name):
+  override def actionPerformed(event: ActionEvent): Unit = ChartDialog(name, panel).open()
