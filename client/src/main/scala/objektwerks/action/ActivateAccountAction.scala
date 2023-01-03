9a28819ede0072f071c9774eb84b7cf09bdf5253
objektwerks <objektwerks@runbox.com>
2023-01-03 08:37:28 -0500

    added imports

--- client/src/main/scala/objektwerks/action/ActivateAccountAction.scala
+++ client/src/main/scala/objektwerks/action/ActivateAccountAction.scala
@@ -3,5 +3,7 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
+import objektwerks.Model
+
 final class ActivateAccountAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = ???
--- client/src/main/scala/objektwerks/action/DeactivateAccountAction.scala
+++ client/src/main/scala/objektwerks/action/DeactivateAccountAction.scala
@@ -3,5 +3,7 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
+import objektwerks.Model
+
 final class DeactivateAccountAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = ???
