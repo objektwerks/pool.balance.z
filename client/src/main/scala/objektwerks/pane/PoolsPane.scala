8be7d7b80d8d45cad482ad90c0b0f3e7eb474624
objektwerks <objektwerks@runbox.com>
2023-01-03 07:49:00 -0500

    added faults action to pools pane

--- client/src/main/scala/objektwerks/pane/PoolsPane.scala
+++ client/src/main/scala/objektwerks/pane/PoolsPane.scala
@@ -7,7 +7,7 @@ import javax.swing.event.{ListSelectionEvent, ListSelectionListener}
 
 import objektwerks.{Context, Model, Pool}
 import objektwerks.Context.*
-import objektwerks.action.{Actions, AddPoolAction, EditPoolAction, ViewAccountAction}
+import objektwerks.action.{Actions, AddPoolAction, EditPoolAction, ViewAccountAction, ViewFaultstAction}
 import objektwerks.table.{ColumnModel, Table, TableModel}
 
 final class PoolsPane extends JPanel:
@@ -24,14 +24,15 @@ final class PoolsPane extends JPanel:
   }
   val tablePane = new JScrollPane(table)
 
-  val addAction = AddPoolAction(Context.add)
-  val editAction = EditPoolAction(Context.edit)
-  val viewAction = ViewAccountAction(Context.account)
-  val actions = Actions(addAction, editAction, viewAction)
+  val addPoolAction = AddPoolAction(Context.add)
+  val editPoolAction = EditPoolAction(Context.edit)
+  val viewAccountAction = ViewAccountAction(Context.account)
+  val viewFaultsAction = ViewFaultstAction("")
+  val actions = Actions(addPoolAction, editPoolAction, viewAccountAction, viewFaultsAction)
 
   def setSelectedId(id: Long): Unit = Model.selectedPoolId.value = id
 
-  def fireEditActionById(id: Long): Unit = editAction.actionPerformed( new ActionEvent(table, ActionEvent.ACTION_PERFORMED, id.toString) )
+  def fireEditActionById(id: Long): Unit = editPoolAction.actionPerformed( new ActionEvent(table, ActionEvent.ACTION_PERFORMED, id.toString) )
   
   setLayout( new BorderLayout() )
 
