b851baa061cd6abe27bb5e57cac51df46ecc5d95
objektwerks <objektwerks@runbox.com>
2023-01-03 08:04:48 -0500

    added actions to account dialog

--- client/src/main/scala/objektwerks/dialog/AccountDialog.scala
+++ client/src/main/scala/objektwerks/dialog/AccountDialog.scala
@@ -3,6 +3,7 @@ package objektwerks.dialog
 import javax.swing.JLabel
 
 import objektwerks.{Account, Context}
+import objektwerks.action.{Actions, CloseAction}
 import objektwerks.form.Form
 
 final class AccountDialog(account: Account) extends Dialog(Context.account):
@@ -14,3 +15,8 @@ final class AccountDialog(account: Account) extends Dialog(Context.account):
       Context.deactivated -> JLabel(account.deactivated)
     )
   )
+
+  val closeAction = CloseAction(Context.close, () => close())
+  val actions = Actions(closeAction)
+
+  add(form, actions)
