11cf057a3baf8bfef0bcd0e4217efc0a88ac86de
objektwerks <objektwerks@runbox.com>
2023-01-03 02:28:52 -0500

    simplified frame

--- client/src/main/scala/objektwerks/Frame.scala
+++ client/src/main/scala/objektwerks/Frame.scala
@@ -1,23 +1,14 @@
 package objektwerks
 
-import java.awt.{GridLayout, Image}
+import java.awt.Image
 import javax.swing.{JFrame, WindowConstants}
-
-object Frame:
-  val rows = 2
-  val columns = 1
-  val horizontalGap = 6
-  val verticalGap = 6
 
 final class Frame(logo: Image,
                   title: String,
                   width: Int,
                   height: Int) extends JFrame:
-  import Frame.*
-
   setIconImage(logo)
   setTitle(title)
-  setLayout( new GridLayout(rows, columns, horizontalGap, verticalGap) )
   setSize(width, height)
   setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
   setLocationRelativeTo(null)
