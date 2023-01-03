10d3cfb4805a490506fc9c9bdbc891b60372e899
objektwerks <objektwerks@runbox.com>
2023-01-03 08:33:23 -0500

    implemented view account action

--- client/src/main/scala/objektwerks/action/ViewAccountAction.scala
+++ client/src/main/scala/objektwerks/action/ViewAccountAction.scala
@@ -3,5 +3,8 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
+import objektwerks.Model
+import objektwerks.dialog.AccountDialog
+
 final class ViewAccountAction(name: String) extends AbstractAction(name):
-  override def actionPerformed(event: ActionEvent): Unit = ???
+  override def actionPerformed(event: ActionEvent): Unit = AccountDialog( Model.observableAccount.get ).open()
