469874179d502acde9225ad6bcf6dfa2806a86b4
objektwerks <objektwerks@runbox.com>
2023-01-04 08:32:28 -0500

    edited login-register dialog

--- client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
+++ client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
@@ -2,7 +2,8 @@ package objektwerks.dialog
 
 import objektwerks.Context
 
+// build login and register form
+// build login and register action
+// build a login and register tab
 final class LoginRegisterDialog extends Dialog(Context.title)
-  // build a login and register tab
-  // build login and register form
-  // build login and register action
+  
