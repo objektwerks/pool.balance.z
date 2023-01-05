bbbe0f3553fa69c78d5948a317b47146e68bb985
objektwerks <objektwerks@runbox.com>
2023-01-05 13:45:55 -0500

    added preferred size to double field

--- client/src/main/scala/objektwerks/field/DoubleField.scala
+++ client/src/main/scala/objektwerks/field/DoubleField.scala
@@ -1,11 +1,13 @@
 package objektwerks.field
 
+import java.awt.Dimension
 import java.beans.PropertyChangeEvent
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
 final class DoubleField(value: Double,
                         fireChangeAction: Double => Unit) extends JFormattedTextField( NumberFormat.getNumberInstance() ):
+  setPreferredSize( Dimension( 100, 40) )
   setValue(value)
   setColumns(10)
 
