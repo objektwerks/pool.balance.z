41f2fcce85c1a14e69c0f4ea6e26c07656bfea86
objektwerks <objektwerks@runbox.com>
2023-01-05 13:46:25 -0500

    added preferred size to int field

--- client/src/main/scala/objektwerks/field/IntField.scala
+++ client/src/main/scala/objektwerks/field/IntField.scala
@@ -1,11 +1,13 @@
 package objektwerks.field
 
+import java.awt.Dimension
 import java.beans.PropertyChangeEvent
 import javax.swing.JFormattedTextField
 import java.text.NumberFormat
 
 final class IntField(value: Int,
                      fireChangeAction: Int => Unit) extends JFormattedTextField( NumberFormat.getIntegerInstance() ):
+  setPreferredSize( Dimension( 100, 40) )
   setValue(value)
   setColumns(10)
 
