3091eedb00eeb2fec8f9ac33f011aea319c3edff
objektwerks <objektwerks@runbox.com>
2023-01-05 08:14:16 -0500

    added register fields and form to login-register dialog

--- client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
+++ client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
@@ -5,6 +5,7 @@ import javax.swing.JTextField
 import objektwerks.{Context, Login}
 import objektwerks.field.StringField
 import objektwerks.form.Form
+import objektwerks.Register
 
 // build login and register form
 // build login and register action
@@ -28,5 +29,19 @@ final class LoginRegisterDialog extends Dialog(Context.title):
     List[(String, JTextField)](
       Context.emailAddress -> loginEmailAddress,
       Context.password -> pin,
+    )
+  )
+
+  var register = Register("")
+
+  val registerEmailAddress = StringField(
+    "",
+    36,
+    (value: String) => register = register.copy(emailAddress = value)
+  )
+
+  val registerForm = Form(
+    List[(String, JTextField)](
+      Context.emailAddress -> registerEmailAddress
     )
   )
