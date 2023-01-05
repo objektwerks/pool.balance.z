ab49cbcf6da163328b4d727ccde387f6e5ee7942
objektwerks <objektwerks@runbox.com>
2023-01-05 08:50:08 -0500

    added emailer.send call to handler register method

--- server/src/main/scala/objektwerks/Handler.scala
+++ server/src/main/scala/objektwerks/Handler.scala
@@ -36,8 +36,10 @@ final case class Handler(store: Store, emailer: Emailer):
 
   private def register(emailAddress: String): Task[Registered] =
     val account = Account(emailAddress = emailAddress)
+    emailer.send(
+      List(account.emailAddress), "Register", "Save pin; then destroy this email!"
+    )
     for
-      // TODO Call Emailer with message!
       id <- store.register(account)
     yield Registered( account.copy(id = id) )
 
