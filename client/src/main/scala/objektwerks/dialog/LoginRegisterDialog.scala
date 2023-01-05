4e4d6f7cfcf6ca600de57d3c1b436424ad91bf3f
objektwerks <objektwerks@runbox.com>
2023-01-05 08:27:37 -0500

    added login actions

--- client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
+++ client/src/main/scala/objektwerks/dialog/LoginRegisterDialog.scala
@@ -1,15 +1,21 @@
 package objektwerks.dialog
 
-import javax.swing.JTextField
+import java.awt.BorderLayout
+import javax.swing.{JDialog, JTextField}
 
 import objektwerks.{Context, Login}
+import objektwerks.action.{Actions, LoginAction, RegisterAction}
 import objektwerks.field.StringField
 import objektwerks.form.Form
 import objektwerks.Register
 
-// build login and register action
 // build a login and register tab
-final class LoginRegisterDialog extends Dialog(Context.title):
+final class LoginRegisterDialog extends JDialog:
+  setTitle(Context.title)
+  setLayout( new BorderLayout() )
+  setLocationRelativeTo(null)
+  setModal(true)
+
   var login = Login("", "")
 
   val loginEmailAddress = StringField(
@@ -30,6 +36,9 @@ final class LoginRegisterDialog extends Dialog(Context.title):
       Context.password -> pin,
     )
   )
+
+  val loginAction = LoginAction(Context.login, login)
+  val loginActions = Actions(loginAction)
 
   var register = Register("")
 
