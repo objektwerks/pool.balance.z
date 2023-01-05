9a926c51b8d8aeedc722b635ee28bc57daeb9912
objektwerks <objektwerks@runbox.com>
2023-01-05 13:44:01 -0500

    added preferred size to string field

--- client/src/main/scala/objektwerks/field/StringField.scala
+++ client/src/main/scala/objektwerks/field/StringField.scala
@@ -1,11 +1,13 @@
 package objektwerks.field
 
+import java.awt.Dimension
 import java.beans.PropertyChangeEvent
 import javax.swing.JTextField
 
 final class StringField(value: String,
                         columns: Int,
                         fireChangeAction: String => Unit) extends JTextField(value):
+  setPreferredSize( Dimension( 100, 40) )
   setText(value)
   setColumns(columns)
 
