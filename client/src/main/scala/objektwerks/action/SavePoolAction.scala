b8d55d3a6f87e01ff854049141e6f26fb9a10b03
objektwerks <objektwerks@runbox.com>
2023-01-25 08:03:08 -0500

    edited save pool action

--- client/src/main/scala/objektwerks/action/SavePoolAction.scala
+++ client/src/main/scala/objektwerks/action/SavePoolAction.scala
@@ -7,5 +7,4 @@ import objektwerks.{Model, Pool}
 
 final class SavePoolAction(name: String,
                            pool: Pool) extends AbstractAction(name):
-  override def actionPerformed(event: ActionEvent): Unit = 
-    if pool.id == 0 then Model.add(pool) else Model.update(pool)
+  override def actionPerformed(event: ActionEvent): Unit = Model.save(pool)
