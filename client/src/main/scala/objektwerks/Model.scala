7bd995e9b1b80a97df33ad8f9cc26242800a7e75
objektwerks <objektwerks@runbox.com>
2023-01-25 07:54:53 -0500

    in model replaced cleaning add/update with save

--- client/src/main/scala/objektwerks/Model.scala
+++ client/src/main/scala/objektwerks/Model.scala
@@ -156,21 +156,16 @@ object Model extends LazyLogging:
         case _ => ()
     )
 
-  def add(cleaning: Cleaning): Unit =
+  def save(cleaning: Cleaning): Unit =
     Proxy.call(
       SaveCleaning(observableAccount.get.license, cleaning),
       (event: Event) => event match
-        case fault @ Fault(_, _) => onFault("Model.add cleaning", cleaning, fault)
-        case CleaningSaved(id) => observableCleanings += cleaning.copy(id = id)
-        case _ => ()
-    )
-
-  def update(cleaning: Cleaning): Unit =
-    Proxy.call(
-      SaveCleaning(observableAccount.get.license, cleaning),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFault("Model.update cleaning", cleaning, fault)
-        case CleaningSaved(id) => observableCleanings.update(observableCleanings.indexOf(cleaning), cleaning)
+        case fault @ Fault(_, _) => onFault("Model.save cleaning", cleaning, fault)
+        case CleaningSaved(id) =>
+          if cleaning.id == 0 then observableCleanings += cleaning.copy(id = id)
+          else observableCleanings.update(observableCleanings.indexOf(cleaning), cleaning)
+          observableCleanings.sort()
+          selectedCleaningId.set(cleaning.id)
         case _ => ()
     )
 
