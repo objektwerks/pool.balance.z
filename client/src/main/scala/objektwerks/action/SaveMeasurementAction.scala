be1f6e133d0f89f1dccc90af285fa8f1330bf349
objektwerks <objektwerks@runbox.com>
2023-01-25 08:02:45 -0500

    edited save measurement action

--- client/src/main/scala/objektwerks/action/SaveMeasurementAction.scala
+++ client/src/main/scala/objektwerks/action/SaveMeasurementAction.scala
@@ -7,5 +7,4 @@ import objektwerks.{Model, Measurement}
 
 final class SaveMeasurementAction(name: String,
                                   measurement: Measurement) extends AbstractAction(name):
-  override def actionPerformed(event: ActionEvent): Unit = 
-    if measurement.id == 0 then Model.add(measurement) else Model.update(measurement)
+  override def actionPerformed(event: ActionEvent): Unit = Model.save(measurement)
