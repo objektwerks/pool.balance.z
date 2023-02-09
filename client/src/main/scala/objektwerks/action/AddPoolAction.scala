783d292ee34c4c82df418ae8803abd288f84a44b
objektwerks <objektwerks@runbox.com>
2023-02-09 07:50:19 -0500

    added license to add pool action

--- client/src/main/scala/objektwerks/action/AddPoolAction.scala
+++ client/src/main/scala/objektwerks/action/AddPoolAction.scala
@@ -3,8 +3,8 @@ package objektwerks.action
 import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
+import objektwerks.{Model, Pool}
 import objektwerks.dialog.PoolDialog
-import objektwerks.Pool
 
 final class AddPoolAction(name: String) extends AbstractAction(name):
-  override def actionPerformed(event: ActionEvent): Unit = PoolDialog(Pool()).open()
+  override def actionPerformed(event: ActionEvent): Unit = PoolDialog(Pool(license = Model.observableAccount.get.license)).open()
