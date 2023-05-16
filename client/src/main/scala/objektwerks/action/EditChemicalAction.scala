6541f2a0b0d64e60a96f6bc188e64a45ae162f21
objektwerks <objektwerks@runbox.com>
2023-05-16 06:39:31 -0400

    removed import

--- client/src/main/scala/objektwerks/action/EditChemicalAction.scala
+++ client/src/main/scala/objektwerks/action/EditChemicalAction.scala
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
 import objektwerks.dialog.ChemicalDialog
-import objektwerks.{Model, Chemical}
+import objektwerks.Model
 
 final class EditChemicalAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = Model.currentChemical.fold(())(chemical => ChemicalDialog(chemical).open())
