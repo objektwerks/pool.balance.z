3e872c41d278735cb895581007cf9e03eb308628
objektwerks <objektwerks@runbox.com>
2023-05-16 06:40:07 -0400

    removed import

--- client/src/main/scala/objektwerks/action/EditMeasurementAction.scala
+++ client/src/main/scala/objektwerks/action/EditMeasurementAction.scala
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
 import objektwerks.dialog.MeasurementDialog
-import objektwerks.{Model, Measurement}
+import objektwerks.Model
 
 final class EditMeasurementAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = Model.currentMeasurement.fold(())(measurement => MeasurementDialog(measurement).open())
