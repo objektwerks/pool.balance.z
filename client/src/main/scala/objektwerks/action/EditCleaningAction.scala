a99d0310c821191009f2eac4a5c3929f3553ed77
objektwerks <objektwerks@runbox.com>
2023-05-16 06:39:48 -0400

    removed import

--- client/src/main/scala/objektwerks/action/EditCleaningAction.scala
+++ client/src/main/scala/objektwerks/action/EditCleaningAction.scala
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent
 import javax.swing.AbstractAction
 
 import objektwerks.dialog.CleaningDialog
-import objektwerks.{Model, Cleaning}
+import objektwerks.Model
 
 final class EditCleaningAction(name: String) extends AbstractAction(name):
   override def actionPerformed(event: ActionEvent): Unit = Model.currentCleaning.fold(())(cleaning => CleaningDialog(cleaning).open())
