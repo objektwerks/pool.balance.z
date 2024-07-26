49f23f86b3d093209a032c44ea6f78c961434efe
objektwerks <objektwerks@runbox.com>
2024-07-26 14:16:27 -0400

    added pin

---
+++ shared/src/main/scala/objektwerks/Pin.scala
@@ -0,0 +1,25 @@
+package objektwerks
+
+import scala.collection.mutable
+import scala.util.Random
+
+object Pin:
+  private val chars = "abcdefghijklmnopqrstuvwxyz"
+  private val specialChars = "~!@#$%^&*-+=<>?/:;"
+  private val random = Random
+
+  private def newChar: Char = chars( random.nextInt(chars.length) )
+  private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
+
+  def newInstance: String =
+    val pin = mutable.ArrayBuffer[Char]()
+
+    pin += newSpecialChar
+    pin += newChar.toUpper
+    pin += newChar.toLower
+    pin += newChar.toUpper
+    pin += newChar.toLower
+    pin += newChar.toUpper
+    pin += newSpecialChar
+
+    pin.mkString
