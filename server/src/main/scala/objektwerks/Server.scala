6740910d50fb910a7695f46c88cd0ad4e12188aa
objektwerks <objektwerks@runbox.com>
2024-07-31 18:08:42 -0400

    edited server

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -7,26 +7,16 @@ import zio.json.{DecoderOps, EncoderOps}
 import Serializer.given
 
 object Server extends ZIOAppDefault:
-  val routes: Routes[Any, Response] = Routes(
+  val routes = Routes(
     Method.POST / "command" -> handler: (request: Request) =>
-        request.body.asString.flatMap { json =>
-          json.fromJson[Command] match
-            case Right(command: Command) =>
-              for
-                _       <- ZIO.log(s"*** Command: $command")
-                handler <- ZIO.service[Handler]
-                event   <- handler
-                            .handle(command)
-                            .catchAll( throwable =>
-                              val message = s"*** Failed to process command: $command due to this error: ${throwable.getMessage}"
-                              ZIO.log(message) zip ZIO.succeed(Fault(message))
-                            )
-                _       <- ZIO.log(s"*** Event: $event")
-              yield Response.json(event.toJson)
-            case Left(error: String) => 
-              val fault = Fault(error)
-              ZIO.log(s"*** Fault: $fault") *> Response.json(fault.toJson)
-        }
+      for
+        body    <- request.body.asString.orDie
+        command <- ZIO.fromEither( body.fromJson[Command] )
+        _       <- ZIO.log(s"*** Command: $command")
+        handler <- ZIO.service[Handler]
+        event   <- handler.handle(command)
+        _       <- ZIO.log(s"*** Event: $event")
+      yield Response.json(event.toJson)
   ).handleError( _ match
     case error: String => Response.json( Fault(s"Invalid json: $error").toJson )
   )
