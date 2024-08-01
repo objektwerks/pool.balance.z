e13e0f4d33590fcace37458daf62787e1e6da97f
objektwerks <objektwerks@runbox.com>
2024-08-01 11:02:00 -0400

    added CanEqual to events

--- shared/src/main/scala/objektwerks/Event.scala
+++ shared/src/main/scala/objektwerks/Event.scala
@@ -4,22 +4,22 @@ import java.time.LocalDate
 
 sealed trait Event
 
-final case class Registered(account: Account) extends Event
-final case class LoggedIn(account: Account) extends Event
+final case class Registered(account: Account) extends Event derives CanEqual
+final case class LoggedIn(account: Account) extends Event derives CanEqual
 
-final case class Deactivated(account: Account) extends Event
-final case class Reactivated(account: Account) extends Event
+final case class Deactivated(account: Account) extends Event derives CanEqual
+final case class Reactivated(account: Account) extends Event derives CanEqual
 
-final case class PoolsListed(pools: List[Pool]) extends Event
-final case class PoolSaved(id: Long) extends Event
+final case class PoolsListed(pools: List[Pool]) extends Event derives CanEqual
+final case class PoolSaved(id: Long) extends Event derives CanEqual
 
-final case class CleaningsListed(cleanings: List[Cleaning]) extends Event
-final case class CleaningSaved(id: Long) extends Event
+final case class CleaningsListed(cleanings: List[Cleaning]) extends Event derives CanEqual
+final case class CleaningSaved(id: Long) extends Event derives CanEqual
 
-final case class MeasurementsListed(measurements: List[Measurement]) extends Event
-final case class MeasurementSaved(id: Long) extends Event
+final case class MeasurementsListed(measurements: List[Measurement]) extends Event derives CanEqual
+final case class MeasurementSaved(id: Long) extends Event derives CanEqual
 
-final case class ChemicalsListed(chemicals: List[Chemical]) extends Event
-final case class ChemicalSaved(id: Long) extends Event
+final case class ChemicalsListed(chemicals: List[Chemical]) extends Event derives CanEqual
+final case class ChemicalSaved(id: Long) extends Event derives CanEqual
 
-final case class Fault(cause: String, occurred: Long = LocalDate.now.toEpochDay) extends Event
+final case class Fault(cause: String, occurred: Long = LocalDate.now.toEpochDay) extends Event derives CanEqual
