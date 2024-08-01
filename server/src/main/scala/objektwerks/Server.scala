0ae679038f1ba0c9351c9b0edbad02827aef6f24
objektwerks <objektwerks@runbox.com>
2024-08-01 10:43:06 -0400

    removed debug

--- server/src/main/scala/objektwerks/Server.scala
+++ server/src/main/scala/objektwerks/Server.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
+import zio.{Console, Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 import zio.http.{handler, Method, Request, Response, Routes}
 import zio.json.{DecoderOps, EncoderOps}
 
@@ -11,11 +11,12 @@ object Server extends ZIOAppDefault:
     Method.POST / "command" -> handler: (request: Request) =>
       for
         body    <- request.body.asString.orDie
+        _       <- Console.printLine(s"*** Json: $body")
         command <- ZIO.fromEither( body.fromJson[Command] )
-        _       <- ZIO.log(s"*** Command: $command")
+        _       <- Console.printLine(s"*** Command: $command")
         handler <- ZIO.service[Handler]
         event   <- handler.handle(command)
-        _       <- ZIO.log(s"*** Event: $event")
+        _       <- Console.printLine(s"*** Event: $event")
       yield Response.json(event.toJson)
   ).handleError( _ match
     case error: String => Response.json( Fault(s"*** Invalid json: $error").toJson )
