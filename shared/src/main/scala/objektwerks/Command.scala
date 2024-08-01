ae63b95d67c55105391305829df1462c1b98579a
objektwerks <objektwerks@runbox.com>
2024-08-01 11:01:08 -0400

    added CanEqual to commands

--- shared/src/main/scala/objektwerks/Command.scala
+++ shared/src/main/scala/objektwerks/Command.scala
@@ -5,20 +5,20 @@ sealed trait Command
 sealed trait License:
   val license: String
 
-final case class Register(emailAddress: String) extends Command
-final case class Login(emailAddress: String, pin: String) extends Command
+final case class Register(emailAddress: String) extends Command derives CanEqual
+final case class Login(emailAddress: String, pin: String) extends Command derives CanEqual
 
-final case class Deactivate(license: String) extends Command with License
-final case class Reactivate(license: String) extends Command with License
+final case class Deactivate(license: String) extends Command with License derives CanEqual
+final case class Reactivate(license: String) extends Command with License derives CanEqual
 
-final case class ListPools(license: String) extends Command with License
-final case class SavePool(license: String, pool: Pool) extends Command with License
+final case class ListPools(license: String) extends Command with License derives CanEqual
+final case class SavePool(license: String, pool: Pool) extends Command with License derives CanEqual
 
-final case class ListCleanings(license: String, poolId: Long) extends Command with License
-final case class SaveCleaning(license: String, cleaning: Cleaning) extends Command with License
+final case class ListCleanings(license: String, poolId: Long) extends Command with License derives CanEqual
+final case class SaveCleaning(license: String, cleaning: Cleaning) extends Command with License derives CanEqual
 
-final case class ListMeasurements(license: String, poolId: Long) extends Command with License
-final case class SaveMeasurement(license: String, measurement: Measurement) extends Command with License
+final case class ListMeasurements(license: String, poolId: Long) extends Command with License derives CanEqual
+final case class SaveMeasurement(license: String, measurement: Measurement) extends Command with License derives CanEqual
 
-final case class ListChemicals(license: String, poolId: Long) extends Command with License
-final case class SaveChemical(license: String, chemical: Chemical) extends Command with License
+final case class ListChemicals(license: String, poolId: Long) extends Command with License derives CanEqual
+final case class SaveChemical(license: String, chemical: Chemical) extends Command with License derives CanEqual
