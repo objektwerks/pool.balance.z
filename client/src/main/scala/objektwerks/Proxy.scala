79683827856ac00b47e8bf7b59b6190eea8be221
objektwerks <objektwerks@runbox.com>
2024-08-02 10:56:40 -0400

    refactoring proxy

--- client/src/main/scala/objektwerks/Proxy.scala
+++ client/src/main/scala/objektwerks/Proxy.scala
@@ -11,24 +11,22 @@ import Serializer.given
 object Proxy extends LazyLogging:
   private val url = Context.url
 
-  /* 
-    val fault = Fault(error)
-    ZIO.succeed( logger.info(s"*** Proxy fault: $fault") ) *>
-    ZIO.succeed( handler(fault) )
-   */
-
   private def delegate(command: Command,
                        handler: Event => Unit) =
-    for
-      _           <- ZIO.succeed( logger.info(s"*** Proxy command: $command") )
-      commandJson =  writeToString[Command](command)
-      request     =  Request.post(url, Body.fromString(commandJson)) // Context.headers?
-      response    <- Client.request(request)
-      eventJson   <- response.body.asString.orDie
-      event       =  readFromString[Event](eventJson)
-      _           <- ZIO.succeed( logger.info(s"*** Proxy event: $event") )
-      _           <- ZIO.succeed( handler(event) )
-    yield ()
+    (
+      for
+        _           <- ZIO.succeed( logger.info(s"*** Proxy command: $command") )
+        commandJson =  writeToString[Command](command)
+        request     =  Request.post(url, Body.fromString(commandJson)) // Context.headers?
+        response    <- Client.request(request)
+        eventJson   <- response.body.asString.orDie
+        event       =  readFromString[Event](eventJson)
+        _           <- ZIO.succeed( logger.info(s"*** Proxy event: $event") ) *> ZIO.succeed( handler(event) )
+      yield ()
+    ).catchAll { error =>
+        val fault = Fault(error.getMessage)
+        ZIO.succeed( logger.info(s"*** Proxy fault: $fault") ) *> ZIO.succeed( handler(fault) )
+    }
 
   def call(command: Command,
            handler: Event => Unit): Unit =
